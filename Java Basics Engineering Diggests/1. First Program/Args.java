aa564bdc1fdd189e12400c15831ea73883e20a5e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-01 08:46:53 +0530

    java
    args

---
+++ Java Basics Engineering Diggests/1. First Program/Args.java
@@ -0,0 +1,7 @@
+public class Args {
+    public static void main(String[] Strings){
+        System.out.println("Ck.....");
+        System.out.println(Strings[0]);
+        System.out.println(Strings[1]);
+    }
+}
