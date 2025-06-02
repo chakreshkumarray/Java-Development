e74e1268c78a4b57649f77af86b4aa04cfc84fdc
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-02 15:41:44 +0530

    Naming Conversion
    Naming Conversion

---
+++ Java Basics Engineering Diggests/3. Naming Conversion/Kebab_Case.java
@@ -0,0 +1,7 @@
+public class Kebab_Case {
+    public static void main(String[] args) {
+
+        String my = "ck..."; // my-variable-name="ck";
+        System.out.println(my);
+    }
+}
---
+++ Java Basics Engineering Diggests/3. Naming Conversion/Snake_Case.java
@@ -0,0 +1,7 @@
+public class Snake_Case {
+    public static void main(String[] args) {
+
+        String my_variable_name = "ck ray...";
+        System.out.println(my_variable_name);
+    }
+}
