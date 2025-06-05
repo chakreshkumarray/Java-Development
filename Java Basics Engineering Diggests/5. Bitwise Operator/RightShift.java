4440879d120d9cc25ea16ce854a5976ab8bca262
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-05 12:59:40 +0530

    bitwsie
    bitwise

---
+++ Java Basics Engineering Diggests/5. Bitwise Operator/RightShift.java
@@ -0,0 +1,13 @@
+public class RightShift {
+    public static void main(String[] args) {
+
+        int a = -8;
+        int b = a >> 1; // ---------->
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+
+    }
+}
