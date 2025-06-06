d44364e5d015b93e173c5cb7ef070aba686a0eab
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-06 20:45:46 +0530

    Strings Methods
    Strings Methods

---
+++ Java Basics Engineering Diggests/8. Strings Methods/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static void main(String[] args) {
+
+        String name="Ck Ray...";
+
+        System.out.println(name.length());
+        System.out.println(name.charAt(2));
+
+
+    }
+}
