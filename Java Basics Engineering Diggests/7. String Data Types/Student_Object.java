ca36a21effb468a2cb1cf008437b3f6b307f49ca
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-06 20:42:08 +0530

    String Pool
    String Pool

---
+++ Java Basics Engineering Diggests/7. String Data Types/Student_Object.java
@@ -0,0 +1,16 @@
+public class Student_Object {
+    public static void main(String[] args) {
+
+        Student_Class student = new Student_Class();
+        student.name = "Chakresh";
+        student.rollNumber = 205423;
+        student.Standard = 12;
+        student.address = "Ahirouli";
+
+        System.out.println(student.name);
+        System.out.println(student.rollNumber);
+        System.out.println(student.Standard);
+        System.out.println(student.address);
+
+    }
+}
