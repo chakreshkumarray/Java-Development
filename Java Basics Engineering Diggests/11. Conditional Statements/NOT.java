6f1532f5d5605d47e776ee62dba3db4c2ac68afa
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-07 15:12:05 +0530

    Conditional Statements
    Conditional Statements

---
+++ Java Basics Engineering Diggests/11. Conditional Statements/NOT.java
@@ -0,0 +1,12 @@
+public class NOT {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        System.out.println(! ( a < b ));
+
+        char c = 'C';
+        char d = 'o';
+        System.out.println(! ( c > d ));
+    }
+}
