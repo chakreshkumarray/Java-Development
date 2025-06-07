7248f122c9e6c7c20b565e65e711fec420b3798a
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-07 15:03:33 +0530

    Relational Operator
    Relational Operator

---
+++ Java Basics Engineering Diggests/9. Relational Operator/NotEqual.java
@@ -0,0 +1,16 @@
+public class NotEqual {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 20;
+        System.out.println(a!=b);
+
+        char c = 'c';
+        char d = 'C';
+        System.out.println(c != d);
+
+        String str1="CKS";
+        String str2="CKs";
+        System.out.println(str1 != str2);
+    }
+}
