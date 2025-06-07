631b4ae10e364f879b91992be39692688fb55ae7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-07 15:04:44 +0530

    AND.java
    Logical Operator

---
+++ Java Basics Engineering Diggests/10. Logical Operator/AND.java
@@ -0,0 +1,16 @@
+public class AND {
+    public static void main(String[] args) {
+
+        int marks=35;
+        int age=25;
+        System.out.println(marks >= 33 && age <= 18);
+
+        boolean a=true;
+        boolean b=false;
+        System.out.println(a && b);
+
+        boolean exp1=2<1;
+        boolean exp2=1<=3;
+        System.out.println(exp1 && exp2);
+    }
+}
