0cb91a6dbfaede423ae8ac485cbad804f157eeea
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-07 15:02:08 +0530

    Equality.java
    Relational Operator

---
+++ Java Basics Engineering Diggests/9. Relational Operator/Equality.java
@@ -0,0 +1,24 @@
+public class Equality {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 20;
+
+        String name = "Chakresh Kumar";
+        String myName = "Chakresh Kumar";
+
+        char c = 'C';
+        char d = 'c';
+
+        String name1 = new String("Chakresh Kumar");
+
+        System.out.println(a == b);
+        System.out.println(myName == name);
+
+        System.out.println(name1 == myName);
+
+        System.out.println('c'+0);
+        System.out.println('C'+0);
+        System.out.println(c == d);
+    }
+}
