d8504b9692d0e2fb945774f483a673a5e7c7250a
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-15 09:02:24 +0530

    BreakInLoop.java
    Loops

---
+++ Java Basics Engineering Diggests/13. Loops/BreakInLoop.java
@@ -0,0 +1,23 @@
+public class BreakInLoop {
+    public static void main(String[] args) {
+
+        int i=0;
+        while (i<1000){
+            System.out.println(i);
+            if(i==501){
+                break;
+            }
+            i++;
+        }
+        System.out.println("ck..");
+
+
+
+        for(int a=1;a<=20;a++){
+            if(a==15){
+                break;
+            }
+            System.out.println(+a);
+        }
+    }
+}
