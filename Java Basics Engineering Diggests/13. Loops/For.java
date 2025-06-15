2b7c91b2ac54b5d7b68ae1e26d868728447ddf06
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-15 09:06:26 +0530

    Loops
    Loops

---
+++ Java Basics Engineering Diggests/13. Loops/For.java
@@ -0,0 +1,19 @@
+public class For {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
+        // pattern printing
+        for(int i=1;i<=100000;i=i*10){
+            System.out.println(i);
+        }
+
+
+
+
+
+
+    }
+}
