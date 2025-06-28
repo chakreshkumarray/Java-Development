b41e8481422711551d1fc15d6f8e23d3882abf2b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-28 08:10:29 +0530

    Packages & ClassPath
    Packages & ClassPath

---
+++ Java Basics Engineering Diggests/18. Packages & ClassPath/Car.java
@@ -0,0 +1,8 @@
+package com.ckray.corejava.vehicals;
+
+public class Car {
+    public static void main(String[] args) {
+        Cycles cycles =new Cycles();
+     Cycles.MiniCycles miniCycle = new Cycles.MiniCycles();
+    }
+}
