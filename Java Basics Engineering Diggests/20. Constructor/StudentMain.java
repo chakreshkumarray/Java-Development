a2e5a781c53489d9c9959e05e94bb47dc693dd0b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-29 10:16:01 +0530

    Constructor
    Constructor

---
+++ Java Basics Engineering Diggests/20. Constructor/StudentMain.java
@@ -0,0 +1,32 @@
+public class StudentMain {
+    public static void main(String[] args) {
+
+        Student student = new Student("ck",15,1,"OS","denis richie");
+        /*
+         *  Student student = new Student();
+           +--------------------+
+           +   Student_Class(); +----------> This is constructor
+           +--------------------+
+
+         default value
+         student.getName();  default value
+         student.getStandard();  default value
+         over load access
+         System.out.println(student.getName()); overload remark ck
+         */
+        System.out.println(student.getName());
+        System.out.println(student.getStandard());
+        System.out.println(student.getAuthor()); // customize constructor print
+        System.out.println(student.getSubject());
+        System.out.println(student.getAge());
+
+        // Adding new object
+        Student student1 = new Student("Raman");
+        System.out.println(student1.getAuthor());
+
+        // Adding also new object
+        Student student2 = new Student(21);
+        System.out.println(student2.getAge());
+
+    }
+}
