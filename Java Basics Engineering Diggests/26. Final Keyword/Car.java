04d73d0fed9bf004cafaee1f60f411661a6be23a
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-06-30 16:28:28 +0530

    Car.java
    Final Keyword

---
+++ Java Basics Engineering Diggests/26. Final Keyword/Car.java
@@ -0,0 +1,37 @@
+public  class  Car extends Vehicle{
+
+    public final void airBag(){
+        System.out.println(" 4 air bags");
+    }
+
+    public static final double PI;
+
+    static {
+        PI = 3.15499;
+    }
+
+    private final int speedLimit=200;
+
+   // public Car{
+     //   speedLimit = 300;
+    //}
+
+  //  public int getSpeedLimit() {
+     //   return speedLimit;
+   // }
+
+   // public void setSpeedLimit(int speedLimit) {
+     //   this.speedLimit = speedLimit;
+    //}
+
+    @Override
+    public void accelerate(){
+
+    }
+
+    @Override
+    public void decelerate(){
+
+    }
+
+}
