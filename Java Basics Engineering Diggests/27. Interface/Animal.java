99be53633e696b4a56ded30a65499f193cc6223f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-01 09:35:59 +0530

    Animal.java
    Interface

---
+++ Java Basics Engineering Diggests/27. Interface/Animal.java
@@ -0,0 +1,10 @@
+package AbstractMethods;
+
+public interface Animal {
+
+    public abstract void eat();    // abstract methods
+    void sleep();
+
+    public static final int MAX_AGE = 23; // Static Constants
+
+}
