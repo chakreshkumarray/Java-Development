77270533edbeb7c9e8e1ef4648f56f6cf2d9643e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-01 22:55:48 +0530

    BankAccount.java
    Exception Handling

---
+++ Java Basics Engineering Diggests/29. Exception Handling/BankAccount.java
@@ -0,0 +1,16 @@
+package Custom_Exceptions;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double amount){
+        this.balance = amount;
+    }
+    public void Withdraw(double amount) throws Exception{
+        if (amount > balance){
+            throw  new LowBalance();
+        }
+        balance = balance-amount;
+    }
+
+}
