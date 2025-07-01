81ba6d0750fbaf9b90673d0d2bc43376a2f3e426
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-01 22:58:36 +0530

    Boxing.java
    Wrapper Classes

---
+++ Java Basics Engineering Diggests/30. Wrapper Classes/Boxing.java
@@ -0,0 +1,13 @@
+package AutoBoxing;
+
+public class Boxing {
+    public static void main(String[] args) {
+
+        int b = 12;
+        Integer a = 1; // AutoBoxing
+        // Behind the seen
+
+        Integer c = Integer.valueOf(12);
+
+    }
+}
