bbae8a5a4e3fbbf5a7f333e2f1ebca1a39174e5c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-12 12:02:57 +0530

    Problem.java
    Generic Class

---
+++ Java Basics Engineering Diggests/35. Generic Class/Problem.java
@@ -0,0 +1,25 @@
+package Topic1_WhyGeneric.WithOutGenerices;
+
+import java.util.ArrayList;
+
+public class Problem {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("hello");
+        list.add(123);
+        list.add(3.14);
+        list.add('c');
+
+        /* problem
+        *Above code has 3 major issues
+
+           No Type safety
+           Manual casting
+           No Compile Time checking
+         */
+
+        String str = (String) list.get(0);
+        String integer = (String) list.get(1);
+        
+    }
+}
