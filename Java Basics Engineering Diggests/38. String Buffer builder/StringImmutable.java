6437cf1438040287dd1ff1f9eb017b1d37070295
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-12 16:19:46 +0530

    StringImmutable.java
    String String Buffer String builder

---
+++ Java Basics Engineering Diggests/38. String Buffer builder/StringImmutable.java
@@ -0,0 +1,15 @@
+package String;
+public class StringImmutable {
+    public static void main(String[] args) {
+        String str1 = "Chakresh";
+
+        //str1.concat("Kumar");
+        String str2 = str1.concat(" Kumar");
+
+        System.out.println(str1);
+
+
+        System.out.println(str2);
+
+    }
+}
