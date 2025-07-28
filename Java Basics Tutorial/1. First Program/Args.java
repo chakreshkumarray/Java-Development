b2e0edc439523c1a59a548c841d7c3bfba86a0c3
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-07-28 11:10:05 +0530

    Delete Java Basics Tutorial/1. First Program directory

--- Java Basics Tutorial/1. First Program/Args.java
+++
@@ -1,7 +0,0 @@
-public class Args {
-    public static void main(String[] Strings){
-        System.out.println("Ck.....");
-        System.out.println(Strings[0]);
-        System.out.println(Strings[1]);
-    }
-}
