ad1e2359e852e207a50d47f7c9d81641a4dc4a3f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-08-21 20:47:27 +0530

    First API
    First API

--- Java Framework SpringBoot/1.Create-First-Spring-Boot-Project-API/Application.java
+++ Java Framework SpringBoot/1.Create-First-Spring-Boot-Project-API/Application.java
@@ -1,1 +1,13 @@
-"E:\JAVA Development\Java Framework SpringBoot\1.Create-First-Spring-Boot-Project-API\src\main\java\com\example\demo\Application.java"
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
