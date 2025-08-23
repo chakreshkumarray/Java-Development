952f2ff27363e4c3ff7e411c071e115d7d5f1a30
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-08-23 20:05:24 +0530

    Topic1_LinearSearchArray.java
    Array

---
+++ Java Data Structure/1. Array/Topic1_LinearSearchArray.java
@@ -0,0 +1,17 @@
+public class Topic1_LinearSearchArray {
+    public static void main(String[] args) {
+        int[]arr = {2,3,1,9};
+        System.out.println(linearSearch(arr,3));
+    }
+
+    public static int linearSearch(int[] arr, int n){
+        for (int  i = 0; i < arr.length; i++){
+            if (arr[i] == n){                            
+                return n;              // Or  i
+            }
+        }
+        return -1;
+    }
+
+
+}
