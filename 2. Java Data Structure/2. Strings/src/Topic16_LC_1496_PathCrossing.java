03db9ea29c814fdf090740e67c32d4cc1be7d869
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-18 20:39:32 +0530

    Update Topic16_LC_1496_PathCrossing.java

--- 2. Java Data Structure/2. Strings/src/Topic16_LC_1496_PathCrossing.java
+++ 2. Java Data Structure/2. Strings/src/Topic16_LC_1496_PathCrossing.java
@@ -1,10 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Topic16_LC_1496_PathCrossing {
     public static void main(String[] args) {
 
+        System.out.println(isPathCrossing("NES"));
+        System.out.println(isPathCrossing("NESWW"));
     }
 
-    public static void isPathCrossing(String path) {
+    public static boolean isPathCrossing(String path) {
+        Set<String> set = new HashSet<>();
+        int x = 0;
+        int y = 0;
+        set.add(0 + "," + 0);
 
+        for (char c: path.toCharArray()){
+            if (c == 'N'){
+                y++;
+            }else if (c == 'W'){
+                x--;
+            } else if (c == 'S') {
+                y--;
+            } else if (c == 'E') {
+                x++;
+            }
+            String coo = x + "," + y;
+            if (set.contains(coo)){
+                return true;
+            }
+            set.add(coo);
+        }
+        return false;
     }
 
 }
