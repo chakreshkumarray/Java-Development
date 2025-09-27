c1935b3982fb126c100754185c0318411de6b8e8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-27 14:33:48 +0530

    Java Prep

--- 1. Java Basics Tutorial/39. Java Interview Preparation/9. Exception Handling and File Handling/src/FileHandling/FileWriters.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/9. Exception Handling and File Handling/src/FileHandling/FileWriters.java
@@ -9,14 +9,6 @@ public class FileWriters {
 
         try (FileWriter writer = new FileWriter(fileName)){
             writer.write("I am Chakresh from sultanpur U.P");
-
-            int fact = 1;
-            for (int i = 1; i <= 5;i++){
-                fact = fact*i;
-            }
-            System.out.println(fact);
-
-            writer.write(fact);
             writer.flush();
             System.out.println("File successfully write");
         } catch (IOException e) {
