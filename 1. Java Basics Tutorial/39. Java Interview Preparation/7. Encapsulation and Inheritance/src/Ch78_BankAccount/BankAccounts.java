499d7eb0db2417358660bafd5ace0b8446bdece3
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 21:18:22 +0530

    java prep

--- 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch78_BankAccount/BankAccounts.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch78_BankAccount/BankAccounts.java
@@ -2,8 +2,26 @@ package Ch78_BankAccount;
 public class BankAccounts {
 
     private String accountNumber;
-    private String accountholder;
+    private String accountHolder;
     private double balance;
+
+    BankAccounts(String accountNumber,String accountHolder,double balance){
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 
     public void depositMoney(double money) {
         if (money <= 0) {
