39a0464cde6e3acd3f29cb2eae577caaa6b6632b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 12:00:47 +0530

    Update Ch61_Table.java

--- 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/Loop/Ch61_Table.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/Loop/Ch61_Table.java
@@ -1,4 +1,16 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class Ch61_Table {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(num+" X "+i+" = "+num*i);
+        }
+    }
 }
