2735ac418a3c409278bed796ce98e56f7a351485
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 20:19:40 +0530

    Update CarMain.java

--- 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Encapsulation/CarMain.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Encapsulation/CarMain.java
@@ -3,6 +3,16 @@ public class CarMain {
     public static void main(String[] args) {
 
         Car car = new Car();
+        car.color = "Black";
+        car.model = "Safari";
+        car.wheel = 4;
+        car.price = 2587999;
+        car.fuelLevel = 4.5;
+        System.out.println(car.color);
+        System.out.println(car.model);
+        System.out.println(car.wheel);
+        System.out.println(car.price);
+        System.out.println(car.fuelLevel);
 
     }
 }
