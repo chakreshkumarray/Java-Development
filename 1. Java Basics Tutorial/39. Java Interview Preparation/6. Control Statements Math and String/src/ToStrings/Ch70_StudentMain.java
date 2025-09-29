23b9825386a9f360e6b22c2d8fa831930cafdca4
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 16:15:11 +0530

    Java Prep

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/ToStrings/Ch70_StudentMain.java
@@ -0,0 +1,9 @@
+package ToStrings;
+public class Ch70_StudentMain {
+    public static void main(String[] args) {
+
+        Student student = new Student("Ck",24,205423,"Ahirouli");
+
+        System.out.println(student);
+    }
+}
