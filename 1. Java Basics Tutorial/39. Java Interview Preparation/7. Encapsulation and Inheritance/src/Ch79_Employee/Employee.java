089ced00bbd361653c8dbdce873426d2557b6653
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 21:31:36 +0530

    java prep

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch79_Employee/Employee.java
@@ -0,0 +1,50 @@
+package Ch79_Employee;
+
+public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public Employee(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee details{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
