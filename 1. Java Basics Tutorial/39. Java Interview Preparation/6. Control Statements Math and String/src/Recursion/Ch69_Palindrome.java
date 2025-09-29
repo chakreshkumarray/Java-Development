fc09a96d8cdfa0091ebcdb1079013c3440e312b4
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 13:24:08 +0530

    Update Ch69_Palindrome.java

--- 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/Recursion/Ch69_Palindrome.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/Recursion/Ch69_Palindrome.java
@@ -1,4 +1,24 @@
 package Recursion;
-
+import java.util.Scanner;
 public class Ch69_Palindrome {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = input.next();
+        System.out.println("Your are: " + (isPalindrome(str) ? "Palindrome" : "Not Palindrome"));
+    }
+
+    public static boolean isPalindrome(String str){
+        if (str.length() <= 1){
+            return true;
+        }
+        int lastPost = str.length()-1;
+        if (str.charAt(0) != str.charAt(lastPost)){
+            return false;
+        }
+        String newStr = str.substring(1,lastPost);
+
+        return isPalindrome(newStr);
+    }
 }
