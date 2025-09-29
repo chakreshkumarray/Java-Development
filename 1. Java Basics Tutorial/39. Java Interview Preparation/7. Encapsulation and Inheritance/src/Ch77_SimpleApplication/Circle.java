37114afd548452b3f76b9bc8fd0e4f9f235e1fff
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-29 20:57:34 +0530

    Java Prep

--- 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch77_SimpleApplication/Circle.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch77_SimpleApplication/Circle.java
@@ -2,4 +2,9 @@ package Ch77_SimpleApplication;
 
 public class Circle {
 
+    public double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
 }
--- 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch77_SimpleApplication/Rectangle.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/7. Encapsulation and Inheritance/src/Ch77_SimpleApplication/Rectangle.java
@@ -1,5 +1,12 @@
 package Ch77_SimpleApplication;
-
 public class Rectangle {
-    
+
+    public int length;
+    public int breadth;
+
+    public Rectangle(int length , int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
 }
