869ee979bc1c2541e45dbf88d09a1d40782d4f42
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-30 16:10:03 +0530

    Java

---
+++ 1. Java Basics Tutorial/6. Print Printf Prinln/src/Print.java
@@ -0,0 +1,9 @@
+public class Print {
+    public static void main(String[] args) {
+
+        System.out.print(1);
+        System.out.print(2.5);
+        System.out.print('y');
+        System.out.print("ck ray....");
+    }
+}
---
+++ 1. Java Basics Tutorial/6. Print Printf Prinln/src/PrintLn.java
@@ -0,0 +1,11 @@
+
+
+public class PrintLn {
+    public static void main(String[] args  ) {
+
+        System.out.println(1);
+        System.out.println('x');
+        System.out.println();
+        System.out.println("Hello, World!");
+    }
+}
