334cca02acb1c7f41a7eb4690be8d7d366e3c39b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-09-30 16:13:17 +0530

    Java

---
+++ 1. Java Basics Tutorial/7. String Data Types/src/StringPool.java
@@ -0,0 +1,17 @@
+public class StringPool {
+    public static void main(String[] args) {
+
+        String a = "ck";  // Stored in String Pool as same memory location
+        String b = "ck";  // Stored in String Pool as same memory location
+
+        String c = new String("ck"); // Stored in Heap created object different address
+        String d = new String("ck"); // Stored in Heap created object different address
+
+        String x = "Ram";
+        String y = new String("Ram");
+
+        System.out.println(a == b); // Reference Checking not Equality
+        System.out.println(c == d);  // Reference Checking not Equality
+        System.out.println(x == y); // Reference Checking not Equality
+    }
+}
---
+++ 1. Java Basics Tutorial/7. String Data Types/src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        String name = "Ck Ray...";
+        String address = new String("India"); // second method -> create object
+        System.out.println(address);
+        System.out.println(name);
+
+
+
+    }
+}
