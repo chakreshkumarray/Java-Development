559a174967348bf310d21a7b40ecfd542afdffa8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-07 14:22:43 +0530

    Update Signup.java

--- Project/Bank Management System/src/bank/management/system/Signup.java
+++ Project/Bank Management System/src/bank/management/system/Signup.java
@@ -243,8 +243,8 @@ public class Signup extends JFrame implements ActionListener {
                 JOptionPane.showMessageDialog(null, "Fill All Field");
             }else {
                 Con con1 = new Con();
-                String q = "insert into signup values('"+formno+"','"+name+"','"+fname+"','"+dob+"','"+gender+"','"+email+"','"
-                        +marital+"','"+address+"','"+city+"','"+pincode+"','"+state+"')";
+                String q = "insert into signup values('"+formno+"','"+name+"','"+fname+"','"+dob+"','"+gender+"','"
+                        +email+"','" +marital+"','"+address+"','"+city+"','"+pincode+"','"+state+"')";
                 con1.statement.executeUpdate(q);
                 new Signup2();
                 setVisible(false);
