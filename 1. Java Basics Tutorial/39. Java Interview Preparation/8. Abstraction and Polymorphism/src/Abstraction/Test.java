3b7ff13cd516b414075d44f66fba12af63300af2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-14 20:04:20 +0530

    Update Test.java

--- 1. Java Basics Tutorial/39. Java Interview Preparation/8. Abstraction and Polymorphism/src/Abstraction/Test.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/8. Abstraction and Polymorphism/src/Abstraction/Test.java
@@ -3,7 +3,11 @@ package Abstraction;
 public class Test {
     public static void main(String[] args) {
 
-        // Vehicle vehicle = new Vehicle();
+        /*
+         * Vehicle vehicle = new Vehicle();
+         *         not able to create object
+         */
+
         Car vehicle = new Car();
         vehicle.accelerate();
         vehicle.Speed();
