2e40a3cdfc02e4ad571162636d895a41e6a0e1ff
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-16 16:40:23 +0530

    Create Message.java

---
+++ 6. Java Servlet/4. Redirect Servlet/src/main/java/com/company/servlet/Message.java
@@ -0,0 +1,26 @@
+package com.company.servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Message extends HttpServlet{
+	
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+		
+		
+		int num1 = Integer.parseInt(request.getParameter("fname"));
+		int num2 = Integer.parseInt(request.getParameter("lname"));
+		int sum = num1 + num2;
+		
+		response.sendRedirect("Addition?number="+sum);
+		
+		
+	}
+	
+	
+
+}
