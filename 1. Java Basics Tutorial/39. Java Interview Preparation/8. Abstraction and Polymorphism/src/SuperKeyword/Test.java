6c75477b0673feeb90f6790db12b5636ac0e9b76
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-17 12:12:56 +0530

    Create Test.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/8. Abstraction and Polymorphism/src/SuperKeyword/Test.java
@@ -0,0 +1,11 @@
+package SuperKeyword;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Child child = new Child(15,"ck."); // argument by super keyword
+        child.childMethod(); // parent method call
+        System.out.println(child.getAge());
+        System.out.println(child.getName());
+    }
+}
