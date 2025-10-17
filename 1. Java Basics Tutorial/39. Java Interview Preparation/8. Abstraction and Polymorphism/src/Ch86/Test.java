2fd37d2ace54350365da0ac1182a3a5a5dea88ee
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-17 21:27:52 +0530

    Create Test.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/8. Abstraction and Polymorphism/src/Ch86/Test.java
@@ -0,0 +1,13 @@
+package Ch86;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.service();
+
+        Car car = new Car();
+        car.service();
+
+    }
+}
