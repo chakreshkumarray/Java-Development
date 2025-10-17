9c65be6b9e881345cd4f2f050d70428d5d6e1be6
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-17 12:38:55 +0530

    Create Cycle.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/8. Abstraction and Polymorphism/src/FinalKeyword/Cycle.java
@@ -0,0 +1,15 @@
+package FinalKeyword;
+
+public class Cycle extends Vehicle{
+
+    @Override
+    public void accelerate() {
+
+    }
+
+    @Override
+    public void decelerate() {
+
+    }
+
+}
