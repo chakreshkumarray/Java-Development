bdfc25ab0407d2435ece0376aa3b18f6cb61b26c
Prajwal-J03 <prajwaljcloud@gmail.com>
2025-10-21 10:06:49 +0530

    Enhance Test.java with Dog class example
    Added example usage of Dog class with comments explaining functionality.

--- 1. Java Basics Tutorial/21. Inheritance/src/Single_Inheritance/Test.java
+++ 1. Java Basics Tutorial/21. Inheritance/src/Single_Inheritance/Test.java
@@ -2,7 +2,18 @@ package Single_Inheritance;
 
 public class Test {
   public static void main(String[] args) {
+    // Creating an instance of Dog class 
+    Dog dog = new Dog("Buddy",5);  // Initializing the dog with name "Buddy" and age 5
+
+    // Displaying the dog's name and age using getter methods
+    System.out.println("Dog's name: "+dog.getName());  
+    System.out.println("Dog's age: "+dog.getAge());
+
+    //Calling the sayHello() method of the dog. Since it has overridden the method of the Animal class, it will print "Wolf"
+    dog.sayHello();  // Output: Wolf
+
+    // Calling the eat() method of the dog. Since Dog class doesn't override eat() method of the Animal class, so Animal class's eat() will be called
+    dog.eat(); // Output: This animal eats food.
     
   }
-  
 }
