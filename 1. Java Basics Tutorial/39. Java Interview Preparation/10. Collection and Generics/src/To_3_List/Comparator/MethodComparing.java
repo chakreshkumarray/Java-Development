2891864822f277d472b220af2976f1c81394fe64
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-31 13:01:15 +0530

    Create MethodComparing.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/10. Collection and Generics/src/To_3_List/Comparator/MethodComparing.java
@@ -0,0 +1,27 @@
+package To_3_List.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MethodComparing {
+    public static void main(String[] args) {
+
+        List<Student> list = new ArrayList<>();
+        list.add(new Student("Alice",3.4));
+        list.add(new Student("Bob", 3.7));
+        list.add(new Student("Charlie", 3.5));
+        list.add(new Student("Akash", 3.9));
+
+        Comparator<Student> comparator = Comparator.comparing(Student::getGpa).reversed()
+                .thenComparing(Student :: getName); // -> getName
+
+        list.sort(comparator);
+        for (Student s : list) {
+            System.out.println(s.getName() + " : " + s.getGpa());
+        }
+
+        Collections.sort(list,comparator);
+        list.sort(comparator);
+    }
+}
