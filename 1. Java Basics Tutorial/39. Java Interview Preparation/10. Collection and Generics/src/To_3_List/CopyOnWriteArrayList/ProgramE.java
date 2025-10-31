6fa970f4698f7b72a1f8567c66476e8c8dc6262b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-31 13:01:48 +0530

    Create ProgramE.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/10. Collection and Generics/src/To_3_List/CopyOnWriteArrayList/ProgramE.java
@@ -0,0 +1,44 @@
+package To_3_List.CopyOnWriteArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ProgramE {
+    public static void main(String[] args) {
+      //  List<String> sharedList = new ArrayList<>();  // Problem Thread-safe Exception generated
+        List<String> sharedList = new CopyOnWriteArrayList<>(); // Solution
+        sharedList.add("item1");
+        sharedList.add("item2");
+        sharedList.add("item3");
+
+        Thread reader = new Thread(()->{
+            try {
+                while (true){
+                    for (String item :sharedList){
+                        System.out.println("Reading item : "+item);
+                        Thread.sleep(100);
+                    }
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Exception in reader thread : "+ e);
+            }
+        });
+
+        Thread writer = new Thread(()->{
+            try {
+                Thread.sleep(500);
+                sharedList.add("item4");
+                System.out.println("Added item4 to the list.");
+
+                Thread.sleep(500);
+                sharedList.remove("item1");
+                System.out.println("Removed item1 from the list.");
+            } catch (InterruptedException e){
+               e.printStackTrace();
+            }
+        });
+
+        reader.start();
+        writer.start();
+
+    }
+}
