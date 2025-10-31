ede2a9a0432e59fc3a64079908c2af54058880d5
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-31 13:00:37 +0530

    Create Elements_Adding.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/10. Collection and Generics/src/To_3_List/ArrayList/Elements_Adding.java
@@ -0,0 +1,25 @@
+package To_3_List.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Elements_Adding {
+    public static void main(String[] args) {
+
+        List <Integer> list = new ArrayList<>();
+        list.add(23);
+        list.add(55);
+        list.add(78);
+
+        // Specific place
+
+        list.add(1,65);
+        System.out.println(list);
+
+        // Add All
+
+        List<Integer> list1 = List.of(4,5,7,89,22,78);
+        list.addAll(list1);
+        System.out.println(list);
+
+    }
+}
