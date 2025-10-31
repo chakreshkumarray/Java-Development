6ec9eb62dcef690519075f3f1b82b6b18167d1ff
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-31 13:02:35 +0530

    Create Constructor.java

---
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/10. Collection and Generics/src/To_3_List/Vector/Constructor.java
@@ -0,0 +1,19 @@
+package To_3_List.Vector;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Constructor {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+
+        Vector<Integer> vector = new Vector<>(linkedList);
+        System.out.println(vector);
+
+    }
+}
