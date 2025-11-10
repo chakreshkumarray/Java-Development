153e7167620ac69d0904524cf119ab9c04749829
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-10 11:36:40 +0530

    Create FirstEnumSet.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Set/EnumSet/FirstEnumSet.java
@@ -0,0 +1,12 @@
+package Set.EnumSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstEnumSet {
+    public static void main(String[] args) {
+
+        Set<String> set = new HashSet<>();
+
+    }
+}
