e8fbd6760adf26707e6cc67237699eb38ca1e58f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-10 11:36:47 +0530

    Create FirstTreeSet.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Set/TreeSet/FirstTreeSet.java
@@ -0,0 +1,14 @@
+package Set.TreeSet;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class FirstTreeSet {
+    public static void main(String[] args) {
+
+        Set<Integer> set = new TreeSet<>();
+        set.add(3);
+
+        System.out.println(set);
+    }
+}
