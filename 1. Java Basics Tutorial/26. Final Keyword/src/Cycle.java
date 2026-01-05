2f6f26284d0a91f677799620e347f5b2f069f09e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-05 14:33:43 +0530

    Update Cycle.java

--- 1. Java Basics Tutorial/26. Final Keyword/src/Cycle.java
+++ 1. Java Basics Tutorial/26. Final Keyword/src/Cycle.java
@@ -2,12 +2,12 @@ public class Cycle extends Vehicle{
 
     @Override
     public void accelerate() {
-
+        System.out.println("Cycle is accelerating");
     }
 
     @Override
     public void decelerate() {
-
+        System.out.println("Cycle is decelerating");
     }
 
 }
