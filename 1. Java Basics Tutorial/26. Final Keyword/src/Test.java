fccf50851a8cf73ff25d0ef0522e801ace27dbc3
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-05 14:33:57 +0530

    Update Test.java

--- 1. Java Basics Tutorial/26. Final Keyword/src/Test.java
+++ 1. Java Basics Tutorial/26. Final Keyword/src/Test.java
@@ -2,7 +2,11 @@ public class Test {
     public static void main(String[] args) {
 
         Car car = new Car();
+        car.accelerate();
 
+        Cycle cycle = new Cycle();
+        cycle.accelerate();
+        cycle.decelerate();
 
     }
 }
