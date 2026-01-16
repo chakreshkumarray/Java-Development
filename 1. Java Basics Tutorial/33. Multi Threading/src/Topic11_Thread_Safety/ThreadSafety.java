c7cf06a1e6abf4da4a5a2fd113a659e8b562d1ad
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-16 15:02:36 +0530

    Update ThreadSafety.java

--- 1. Java Basics Tutorial/33. Multi Threading/src/Topic11_Thread_Safety/ThreadSafety.java
+++ 1. Java Basics Tutorial/33. Multi Threading/src/Topic11_Thread_Safety/ThreadSafety.java
@@ -6,10 +6,18 @@ public class ThreadSafety {
             When does anything become thread safe?
 
     Answer:
-          When multiple threads are trying to access the system,
+          When multiple threads are trying to access the system(resource),
           and unexpected result,or race condition or data corruption,
           is not there yet ... , does not happening .
           Then we can say this is thread safe ...
 
+     */
+    /*
+    Thread Safety (Java):-
+    Thread safety means a program behaves correctly when multiple threads access shared data at the same time.
+    A thread-safe code prevents race conditions, data inconsistency, and unexpected results.
+
+    Why Thread Safety Is Needed:-
+    When multiple threads modify the same variable without control, results become unpredictable.
      */
 }
