e636f345467a589946cdbd1e366fedc9af2454e2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-17 17:08:33 +0530

    Create DependentService.java

---
+++ 1. Java Basics Tutorial/33. Multi Threading/src/Topic15_CountDownLatch/DependentService.java
@@ -0,0 +1,14 @@
+package Topic15_CountDownLatch;
+
+import java.util.concurrent.Callable;
+
+public class DependentService implements Callable<String> {
+
+        public String call() throws Exception {
+            System.out.println(Thread.currentThread().getName()+ " Service started..");
+            Thread.sleep(2000);
+            return "ck";
+        }
+
+    }
+
