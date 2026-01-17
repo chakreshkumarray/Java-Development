31656a1c3310aa7322f64a90a6217a81f313543b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-17 17:09:12 +0530

    Create CTF3.java

---
+++ 1. Java Basics Tutorial/33. Multi Threading/src/Topic17_CompleteTableFuture/CTF3.java
@@ -0,0 +1,23 @@
+package Topic17_CompleteTableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+
+public class CTF3 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Executor executor = Executors.newFixedThreadPool(3);
+        CompletableFuture<String> future4 = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(2000);
+                System.out.println(" Worker1 ");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } ;
+            return " ck1 ";
+        },executor ).orTimeout(1, TimeUnit.SECONDS).exceptionally(s->"Time Out");
+
+        //.thenApply(x->x+x);  // .get();
+
+        System.out.println(future4.get());
+    }
+}
