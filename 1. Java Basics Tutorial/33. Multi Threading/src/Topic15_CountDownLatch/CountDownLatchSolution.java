567e24a9a78269e922c8f5eda61eecc2f237736b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-17 17:08:37 +0530

    Create CountDownLatchSolution.java

---
+++ 1. Java Basics Tutorial/33. Multi Threading/src/Topic15_CountDownLatch/CountDownLatchSolution.java
@@ -0,0 +1,21 @@
+package Topic15_CountDownLatch;
+import java.util.concurrent.*;
+
+public class CountDownLatchSolution {
+    public static void main(String[] args) throws InterruptedException {
+        int numberOfServices = 3;
+        ExecutorService executorServices =  Executors.newFixedThreadPool(numberOfServices);
+        CountDownLatch latch = new CountDownLatch(numberOfServices);
+        executorServices.submit(new DependentServices(latch));
+        executorServices.submit(new DependentServices(latch));
+        executorServices.submit(new DependentServices(latch));
+        latch.await();
+        latch.await(5 , TimeUnit.SECONDS);
+
+        System.out.println("Main");
+        executorServices.shutdown();
+
+    }
+}
+
+
