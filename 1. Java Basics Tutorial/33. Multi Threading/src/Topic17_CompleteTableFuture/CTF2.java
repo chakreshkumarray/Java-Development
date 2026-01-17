bd2bf5340fa80efc70673dc39f7f6bc13a07f589
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-17 17:09:16 +0530

    Create CTF2.java

---
+++ 1. Java Basics Tutorial/33. Multi Threading/src/Topic17_CompleteTableFuture/CTF2.java
@@ -0,0 +1,24 @@
+package Topic17_CompleteTableFuture;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class CTF2 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+     CompletableFuture<String> future3 = CompletableFuture.supplyAsync(()->{
+            try {
+                Thread.sleep(2000);
+                System.out.println(" Worker1 ");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } ;
+            return " ck1 ";
+        }).orTimeout(1, TimeUnit.SECONDS).exceptionally(s->"Time Out");
+
+             //.thenApply(x->x+x);  // .get();
+
+        System.out.println(future3.get());
+
+    }
+}
