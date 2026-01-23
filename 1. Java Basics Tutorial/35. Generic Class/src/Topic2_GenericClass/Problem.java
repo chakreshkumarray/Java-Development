b4a37d940f4d9a1c47e17e2821a33d1991305aa0
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-23 11:50:07 +0530

    Create Problem.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic2_GenericClass/Problem.java
@@ -0,0 +1,14 @@
+package Topic2_GenericClass;
+
+public class Problem {
+
+    private Object value;
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
