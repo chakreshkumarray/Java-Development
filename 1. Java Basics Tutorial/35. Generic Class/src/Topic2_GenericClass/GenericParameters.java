6235a04fff3a0049614dee42c95819a49eef873c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-23 11:50:02 +0530

    Create GenericParameters.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic2_GenericClass/GenericParameters.java
@@ -0,0 +1,19 @@
+package Topic2_GenericClass;
+
+public class GenericParameters<K,V>{
+    private final K key;
+    private final V value;
+
+    public GenericParameters(K key , V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
