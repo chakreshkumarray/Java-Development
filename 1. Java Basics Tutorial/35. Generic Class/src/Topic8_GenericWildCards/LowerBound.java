cc21d26789c04c39a0036f368e0ea5613f4b177d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:32:54 +0530

    Create LowerBound.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic8_GenericWildCards/LowerBound.java
@@ -0,0 +1,32 @@
+package Topic8_GenericWildCards;
+import java.util.Arrays;
+import java.util.List;
+
+public class LowerBound {
+    public static void main(String[] args) {
+
+        List<? extends Number> numbers = Arrays.asList(2,1,2,1,5,4);
+        // numbers.add(20);   error not able to add
+        List<? super Integer> numbers1 = Arrays.asList(1,1,5,1,6,1,6);
+        numbers1.add(21215);
+        numbers1.add(null);
+
+    }
+
+    // Upper Bound
+    public static double SUM(List < ? extends Number > numbers){
+
+        double sum = 0;
+        for (Number o : numbers){
+            sum = sum + o.doubleValue();
+        }
+        return  sum;
+    }
+
+    // Lower Bound
+    public static void printNumbers(List <? super Integer> list){
+        for (Object obj : list){
+            System.out.println(obj);
+        }
+    }
+}
