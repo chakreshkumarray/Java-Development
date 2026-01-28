16c6db98d6da5b051beb38bab2a6bc170d59fbc4
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:34:37 +0530

    Delete StringProcessingException.java

--- 1. Java Basics Tutorial/35. Generic Class/src/Topic10_GenericException/Work_Round/StringProcessingException.java
+++
@@ -1,9 +0,0 @@
-package Topic10_GenericException.Work_Round;
-
-public class StringProcessingException extends Exception{
-
-    public StringProcessingException(String message){
-        super(message);
-    }
-
-}
