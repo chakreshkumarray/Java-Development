f7d0287329d3e87d7b2455dcef65c08f7a047d6e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:33:11 +0530

    Update SolutionTest.java

--- 1. Java Basics Tutorial/35. Generic Class/src/Topic2_GenericClass/SolutionTest.java
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic2_GenericClass/SolutionTest.java
@@ -3,7 +3,7 @@ package Topic2_GenericClass;
 public class SolutionTest {
     public static void main(String[] args) {
 
-        Solution<Integer> counter = new Solution<Integer>();        // Box is now type-safe
+        Solution<Integer> counter = new Solution<Integer>();        // MultipleBoundedBox is now type-safe
         counter.setValue(205423);                                   // No issue, it's an Integer
         Integer i = counter.getValue();                            // No casting needed
         System.out.println(i);
