1ceb047faa35d36d1e8253c369d621fa3d2f9b4e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:33:19 +0530

    Create Box.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic9_TypeErasure/Box.java
@@ -0,0 +1,14 @@
+package Topic9_TypeErasure;
+
+public class Box {
+
+    private Object value;
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
