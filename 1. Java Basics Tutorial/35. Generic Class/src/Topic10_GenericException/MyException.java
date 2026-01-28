97b9d6594ee9bbe1526317f0fbe6f40c9bb36cd9
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:33:59 +0530

    Create MyException.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic10_GenericException/MyException.java
@@ -0,0 +1,13 @@
+package Topic10_GenericException;
+
+public class MyException extends Exception{
+
+    public <T> MyException (T value){
+        super(
+                "Exception related to value : "+
+                        value.toString() +
+                        " of type " +
+                        value.getClass().getName()
+        );
+    }
+}
