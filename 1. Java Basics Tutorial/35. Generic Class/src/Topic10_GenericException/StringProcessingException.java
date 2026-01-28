83410ba26a8daf69fa732ca6fe7179e7772082d4
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:30:28 +0530

    Create StringProcessingException.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic10_GenericException/StringProcessingException.java
@@ -0,0 +1,9 @@
+package Topic10_GenericException;
+
+public class StringProcessingException extends Exception{
+
+    public StringProcessingException(String message){
+        super(message);
+    }
+
+}
