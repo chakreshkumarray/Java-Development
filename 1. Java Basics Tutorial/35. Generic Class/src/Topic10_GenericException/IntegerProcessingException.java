e370bfcb45f5125a383a03c6ab0cc9d542de6ea1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-01-28 17:30:21 +0530

    Create IntegerProcessingException.java

---
+++ 1. Java Basics Tutorial/35. Generic Class/src/Topic10_GenericException/IntegerProcessingException.java
@@ -0,0 +1,9 @@
+package Topic10_GenericException;
+
+public class IntegerProcessingException extends Exception{
+
+    public IntegerProcessingException(String message ){
+        super(message);
+    }
+
+}
