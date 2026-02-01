32ab038528a2057b73dca10c218649bd50209641
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-01 07:10:20 +0530

    Update Ch62_PrimeNumber.java

--- 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/Loop/Ch62_PrimeNumber.java
+++ 1. Java Basics Tutorial/39. Java Interview Preparation/6. Control Statements Math and String/src/Loop/Ch62_PrimeNumber.java
@@ -9,7 +9,7 @@ public class Ch62_PrimeNumber {
 
     public static boolean isPrime(int num){
         for (int i = 2; i < num; i++){
-            if (num % 2 == 0){
+            if (num % i == 0){
                 return false;
             }
         }
