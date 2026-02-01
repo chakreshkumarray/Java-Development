ac8e1af74da9fd66cf620c06ed14cf2b4d730d38
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-01 07:09:10 +0530

    Create ApplicationTests.java

---
+++ 4. Java Spring Boot/2.CRUD-Operation/src/test/java/com/SpringApp2/CRUD_Operation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringApp2.CRUD_Operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
