0313199a81926d33f75efb3f0bcb0086811029bb
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-01 07:10:31 +0530

    Create ApplicationTests.java

---
+++ 4. Java Spring Boot/1.First-SpringBoot-Program/src/test/java/com/SpringBootApp1/First_SpringBoot_Program/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootApp1.First_SpringBoot_Program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
