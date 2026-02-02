debcafe60539f1916c150ec2016351f49c2473fd
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-02 17:09:58 +0530

    Create Application.java

---
+++ 4. Java Spring Boot/3.CRUD_Operation_With_MongoDB/src/main/java/com/SpringApp3/CRUD_Operation_With_MongoDB/Application.java
@@ -0,0 +1,12 @@
+package com.SpringApp3.CRUD_Operation_With_MongoDB;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
