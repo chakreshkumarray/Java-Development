5ed8e7929531c5122252b553a72c805bc9e6c98f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-04 22:33:14 +0530

    Update FirstEnumSet.java

--- 1. Java Basics Tutorial/36. Collection Framework/src/Set/EnumSet/FirstEnumSet.java
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Set/EnumSet/FirstEnumSet.java
@@ -1,12 +1,13 @@
 package Set.EnumSet;
 
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FirstEnumSet {
     public static void main(String[] args) {
 
-        Set<String> set = new HashSet<>();
+
 
     }
 }
