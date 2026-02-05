b8c2c349b71704fddeaf718c2c24bc6ff226aad7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-05 06:58:22 +0530

    Create Product.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Set/TreeSet/Product.java
@@ -0,0 +1,18 @@
+package Set.TreeSet;
+
+public class Product {
+
+    String name;
+    int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": $" + price;
+    }
+
+}
