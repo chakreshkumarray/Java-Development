b793aaae432b5f8995f0ffc1eca0530310e6dd86
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-05 06:57:47 +0530

    Create Employee.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Set/SortedSet/Employee.java
@@ -0,0 +1,13 @@
+package Set.SortedSet;
+
+public class Employee {
+
+    int id;
+    String name;
+
+    Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
