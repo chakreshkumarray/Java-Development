98c0e25c3ec6daec34cbd377179a8e6eae167880
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-06 13:34:35 +0530

    Create Functionality.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Queues/Deque/LinkedListDeque/Functionality.java
@@ -0,0 +1,36 @@
+package Queues.Deque.LinkedListDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Functionality {
+    public static void main(String[] args) {
+        Deque<Integer> list = new LinkedList<>();
+
+        // Add First , Add Last
+        list.addFirst(2);
+        list.addLast(3);
+
+        // Offer First , Offer Last
+        list.offerFirst(9);
+        list.offerLast(7);
+
+        // Remove First , Remove Last
+        list.removeFirst();
+        list.removeLast();
+
+        // Poll First , Poll Last
+        list.pollFirst();
+        list.pollLast();
+
+        // Get First , Get Last
+        list.getFirst();
+        list.getLast();
+
+        // Peek First , Peek Last
+        list.peekFirst();
+        list.peekLast();
+
+        System.out.println(list);
+
+    }
+}
