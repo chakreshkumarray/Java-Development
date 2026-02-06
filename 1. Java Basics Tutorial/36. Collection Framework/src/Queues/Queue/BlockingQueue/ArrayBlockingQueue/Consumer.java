17f6473831ca3c1f0c75575622f685ea8f2e94c1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-06 13:34:40 +0530

    Create Consumer.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Queues/Queue/BlockingQueue/ArrayBlockingQueue/Consumer.java
@@ -0,0 +1,26 @@
+package Queues.Queue.BlockingQueue.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable{
+
+    private BlockingQueue<Integer> queue;
+
+    public Consumer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+                Integer value = queue.take();
+                System.out.println("Consumer consumed : " +value);
+                Thread.sleep(2000);
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                System.out.println("Consumer Interrupted");
+            }
+        }
+
+    }
+}
