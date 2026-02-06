382e3a77b5c14b04679bd2ef4dda44809d8eebca
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-06 13:35:00 +0530

    Create Create.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/Queues/Queue/BlockingQueue/SynchronousQueue/Create.java
@@ -0,0 +1,32 @@
+package Queues.Queue.BlockingQueue.SynchronousQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+public class Create {
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new SynchronousQueue<>();
+        Thread producer = new Thread(()->{
+            try {
+                System.out.println("Producer is waiting to transfer. ");
+                queue.put("Hello from producer ! ");
+                System.out.println("Producer has transfer the message..");
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Producer was Interrupted.");
+            }
+        });
+
+        Thread consumer = new Thread(()->{
+            try {
+                System.out.println("Consumer is waiting to receive. ");
+                String message = queue.take();
+                System.out.println("Consumer received : "+ message);
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Consumer was Interrupted.");
+            }
+        });
+        producer.start();
+        consumer.start();
+    }
+}
