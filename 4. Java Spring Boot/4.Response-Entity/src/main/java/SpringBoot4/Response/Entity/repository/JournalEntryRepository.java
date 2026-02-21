a0095016acb459f7a280968d8189a0f525a07ef2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-21 20:36:14 +0530

    Create JournalEntryRepository.java

---
+++ 4. Java Spring Boot/4.Response-Entity/src/main/java/SpringBoot4/Response/Entity/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package SpringBoot4.Response.Entity.repository;
+import SpringBoot4.Response.Entity.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
