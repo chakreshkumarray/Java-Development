ff8f05e64d12bd064f91d875fc728319dab45268
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-21 18:13:02 +0530

    Create ApplicationTests.java

---
+++ 4. Java Spring Boot/4.Response-Entity/src/test/java/SpringBoot4/Response/Entity/ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot4.Response.Entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
