c2b1cb9f3c90fb98e34b00e76db08bf2b5ff8008
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 04:14:40 +0530

    Update L27_PrimeNumber.java

--- 1. Java Basics Tutorial/13. Loop/src/L27_PrimeNumber.java
+++ 1. Java Basics Tutorial/13. Loop/src/L27_PrimeNumber.java
@@ -4,6 +4,23 @@ public class L27_PrimeNumber {
         System.out.println(isPrime(11));
         System.out.println(isPrime(12));
         System.out.println(isPrime(23));
+
+        // 1 to 100
+        int temp = 0;
+        int num = 101;
+        for (int i = 1; i <= num; i++){
+
+            for (int j = 2; j < i; j++){
+                if ( i % j == 0){
+                    temp = temp + 1;
+                }
+            }
+            if (temp == 0){
+                System.out.println(i);
+            }else {
+                temp = 0;
+            }
+        }
     }
 
     public static boolean isPrime(int num){
