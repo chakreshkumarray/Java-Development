0354c36bc60a7c74909ca9a129d4bb9fb4055483
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 04:15:01 +0530

    Create L8_CountDigitOfNumber.java

---
+++ 1. Java Basics Tutorial/13. Loop/src/L8_CountDigitOfNumber.java
@@ -0,0 +1,16 @@
+public class L8_CountDigitOfNumber {
+    public static void main(String[] args) {
+
+        // number is 45892383
+        // find digits
+
+        int n = 46845;
+        int result = 0;
+        while (n > 0){
+            n = n / 10;
+            result++;
+        }
+        System.out.println(result);
+
+    }
+}
