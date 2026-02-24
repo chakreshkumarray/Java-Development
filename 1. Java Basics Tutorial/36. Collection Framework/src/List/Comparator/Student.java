c734fec92ea0250fb70deeada2452038f984d13c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 02:13:18 +0530

    Create Student.java

---
+++ 1. Java Basics Tutorial/36. Collection Framework/src/List/Comparator/Student.java
@@ -0,0 +1,26 @@
+package List.Comparator;
+
+class Student {
+    private final String name;
+    private final double gpa;
+
+    public Student(String name, double gpa) {
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    // Optional: Overriding toString makes printing easier
+    @Override
+    public String toString() {
+        return name + " : " + gpa;
+    }
+
+}
