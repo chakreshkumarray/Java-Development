d46af8ef67dfc1896db9dec646d14f2f4df656b9
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 15:56:35 +0530

    Update L48_NestPattern.java

--- 1. Java Basics Tutorial/13. Loop/src/L48_NestPattern.java
+++ 1. Java Basics Tutorial/13. Loop/src/L48_NestPattern.java
@@ -13,5 +13,29 @@ public class L48_NestPattern {
                      *
 
          */
+
+        int n = 5;
+
+        for (int i = -n + 1; i < n; i++) {
+
+            int absI = Math.abs(i);
+            int row = n - absI;
+
+            for (int j = 0; j < 2 * absI; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            if (row > 1) {
+                for (int j = 0; j < 4 * (row - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
     }
 }
