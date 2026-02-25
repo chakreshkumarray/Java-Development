28090b27764a4b1bea95476f1f2230d03b2b827f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 15:56:17 +0530

    Create L57_NumericPattern.java

---
+++ 1. Java Basics Tutorial/13. Loop/src/L57_NumericPattern.java
@@ -0,0 +1,23 @@
+public class L57_NumericPattern {
+    public static void main(String[] args) {
+
+        // 1
+        // 2 6
+        // 3 7 10
+        // 4 8 11 13
+        // 5 9 12 14 15
+
+        for (int i = 1; i <= 5; i++){
+
+            int no = i;
+
+            for (int j = 1; j <= i; j++){
+                System.out.print(no + " ");
+                no = no + 5 - j;
+            }
+
+            System.out.println();
+        }
+
+    }
+}
