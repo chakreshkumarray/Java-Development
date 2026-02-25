888fa136613048d63a284f600419aaf51fc8697f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 11:06:00 +0530

    Create L53_NumericPattern.java

---
+++ 1. Java Basics Tutorial/13. Loop/src/L53_NumericPattern.java
@@ -0,0 +1,10 @@
+public class L53_NumericPattern {
+    public static void main(String[] args) {
+
+        // 1
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13 14 15
+    }
+}
