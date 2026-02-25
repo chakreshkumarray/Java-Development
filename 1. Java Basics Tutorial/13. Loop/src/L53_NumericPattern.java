026aa2ee33fc3b8d699cd25423628a7c0ea735e7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 15:56:31 +0530

    Update L53_NumericPattern.java

--- 1. Java Basics Tutorial/13. Loop/src/L53_NumericPattern.java
+++ 1. Java Basics Tutorial/13. Loop/src/L53_NumericPattern.java
@@ -6,5 +6,14 @@ public class L53_NumericPattern {
         // 4 5 6
         // 7 8 9 10
         // 11 12 13 14 15
+
+        int count = 0;
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= i; j++){
+                count++;
+                System.out.print(count+" ");
+            }
+            System.out.println();
+        }
     }
 }
