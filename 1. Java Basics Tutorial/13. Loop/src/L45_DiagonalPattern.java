1ea8bb25fce39218ac0f62a68ba721ef04aec95f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-25 15:56:38 +0530

    Update L45_DiagonalPattern.java

--- 1. Java Basics Tutorial/13. Loop/src/L45_DiagonalPattern.java
+++ 1. Java Basics Tutorial/13. Loop/src/L45_DiagonalPattern.java
@@ -10,17 +10,18 @@ public class L45_DiagonalPattern {
         //     *
         //   *
         // *
-         // update code
 
-        for (int i = 1; i <= 5; i++){
-            for (int j = 1; j <= i; j++){
-                if (i >= 2 && j <= i - 1){
-                    System.out.print(" ");
-                }else {
-                    System.out.print("*");
-                }
+        int n = 5;
+
+        for (int i = 1; i < n * 2; i++) {
+
+            int spaces = (i <= n) ? (i - 1) : (n * 2 - i - 1);
+
+            for (int j = 0; j < spaces; j++) {
+                System.out.print("  ");
             }
-            System.out.println();
+
+            System.out.println("*");
         }
 
     }
