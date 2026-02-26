ba3c3de6c4bcf90e18200c45ed84a79aa2b18c43
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-26 07:36:55 +0530

    Create A3_AccessByLoop.java

---
+++ 1. Java Basics Tutorial/14. Array/src/A3_AccessByLoop.java
@@ -0,0 +1,10 @@
+public class A3_AccessByLoop {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 6, 5, 4, 9, 3, 3};
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
