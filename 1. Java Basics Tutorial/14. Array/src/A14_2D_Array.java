a5449a569c38292b9e46289669e192a0d11ecc45
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-26 07:37:27 +0530

    Create A14_2D_Array.java

---
+++ 1. Java Basics Tutorial/14. Array/src/A14_2D_Array.java
@@ -0,0 +1,13 @@
+public class A14_2D_Array {
+    public static void main(String[] args) {
+        
+        // declaration
+        int[][] a;
+
+
+        int[][] arr = new int[4][3]; // Heap
+
+        // Stack
+        int[][] num = {{1,3,4},{2,5,6},{6,7,5}};
+    }
+}
