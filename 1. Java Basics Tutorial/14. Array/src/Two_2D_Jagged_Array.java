ea6289d7ceca5b5c0723ffede86fc839a54d8b1b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-26 07:37:49 +0530

    Update Two_2D_Jagged_Array.java

--- 1. Java Basics Tutorial/14. Array/src/Two_2D_Jagged_Array.java
+++ 1. Java Basics Tutorial/14. Array/src/Two_2D_Jagged_Array.java
@@ -1,7 +1,7 @@
 public class Two_2D_Jagged_Array {
     public static void main(String[] args) {
 
-        // Jagged Array column and row length of array different
+        // Jagged A2_Array column and row length of array different
         
         int [][] arr={{2,5,8},{2,4},{3,6,8,9},{7},{9,0,3,8,6}};
         for(int i=0;i< arr.length;i++){
