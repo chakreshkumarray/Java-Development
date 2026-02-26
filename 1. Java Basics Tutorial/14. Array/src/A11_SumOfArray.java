314f6185d9cb58d7b187521ca114e646241fd96d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-26 07:37:12 +0530

    Create A11_SumOfArray.java

---
+++ 1. Java Basics Tutorial/14. Array/src/A11_SumOfArray.java
@@ -0,0 +1,12 @@
+public class A11_SumOfArray {
+    public static void main(String[] args) {
+
+        int[] arr = {54, 32, 58, 46, 12, 36, 78, 65, 32};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+
+    }
+}
