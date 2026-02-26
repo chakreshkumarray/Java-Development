d7d95e67adab9c78e173a12b695fc07713ab35c7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-26 07:35:23 +0530

    Create A8_Search_Element.java

---
+++ 1. Java Basics Tutorial/14. Array/src/A8_Search_Element.java
@@ -0,0 +1,12 @@
+public class A8_Search_Element {
+    public static void main(String[] args) {
+
+        int[] arr = {16, 25, 51, 77, 108};
+        for (int i : arr) {
+            if (i == 77) {
+                System.out.println("Found");
+            }
+        }
+
+    }
+}
