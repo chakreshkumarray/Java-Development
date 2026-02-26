14c2bd3a89d9489af2e7424ac1e9ca65e7cd59ab
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-26 07:35:19 +0530

    Create A6_ArrayReverseElement.java

---
+++ 1. Java Basics Tutorial/14. Array/src/A6_ArrayReverseElement.java
@@ -0,0 +1,11 @@
+public class A6_ArrayReverseElement {
+    public static void main(String[] args) {
+
+        int[] arr = {34, 55, 89, 67, 25, 108, 73};
+
+        for (int i = arr.length-1; i >= 0; i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
