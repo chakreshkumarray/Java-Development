cdea9daf23cdb5c993a40d123aaeba054b512f3e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-27 20:02:34 +0530

    Create A20_TraverseArray.java

---
+++ 1. Java Basics Tutorial/14. Array/src/A20_TraverseArray.java
@@ -0,0 +1,30 @@
+public class A20_TraverseArray {
+    public static void main(String[] args) {
+
+        // There are four ways which we can use to print array
+
+        int[] a = {10, 20, 30, 40};
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+
+        int[][] b = {{10, 20, 30},{40, 50, 60}};
+        for (int i = 0; i < b.length; i++){
+            for (int j = 0; j < b[i].length; j++){
+                System.out.println(b[i][j]);
+            }
+            System.out.println();
+        }
+
+        int[][][] arr = {{{10, 20},{30, 40, 50}},{{60, 70, 80},{90, 100}}};
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                for (int k = 0; k < arr[i][j].length; k++){
+                    System.out.print(arr[i][j][k]);
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
