e2c67583301253a59777b1ba7109ee1fef62ceb8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-28 07:40:22 +0530

    update

--- 2. Java Data Structure/1. Arrays/src/A1_BubbleSort.java
+++ 2. Java Data Structure/1. Arrays/src/A1_BubbleSort.java
@@ -1,6 +1,5 @@
 public class A1_BubbleSort {
     public static void main(String[] args) {
-
         /*
         📌 What is Bubble Sort?
             Bubble Sort is a simple comparison-based sorting algorithm where:
@@ -9,18 +8,34 @@ public class A1_BubbleSort {
             Largest element “bubbles up” to the end in each pass
 
         📌 Time Complexity
-            Best Case (Already Sorted):	O(n) (with optimization)
-            Average Case:	O(n²)
-            Worst Case:	O(n²)
+            Best Case (Already Sorted):	O(n) (with optimization), Average Case:	O(n²). Worst Case:O(n²)
             Space Complexity: → O(1) (In-place sorting)
 
-        📌 Where use:
-            in college for teaching student
-
-        📌 When use :
-             for small data set array size(5- 20)
+        📌 Step by Step:-
+            🔢 Given Array:[5, 1, 4, 2, 8]
+            🔁 Pass 1
+            Compare adjacent elements:
+            (5, 1) → swap → [1, 5, 4, 2, 8]
+            (5, 4) → swap → [1, 4, 5, 2, 8]
+            (5, 2) → swap → [1, 4, 2, 5, 8]
+            (5, 8) → no swap
+            ✅ After Pass 1:
+            [1, 4, 2, 5, 8]
+            👉 Largest element 8 fixed at last position.
+            🔁 Pass 2
+            (1, 4) → no swap
+            (4, 2) → swap → [1, 2, 4, 5, 8]
+            (4, 5) → no swap
+            ✅ After Pass 2:
+            [1, 2, 4, 5, 8]
+            👉 Second largest 5 fixed.
+            🔁 Pass 3
+            (1, 2) → no swap
+            (2, 4) → no swap
+            ✅ No swaps → Array is sorted.
+            🎯 Final Sorted Array
+            [1, 2, 4, 5, 8]
          */
-
         int[] arr = {12, 11, 50, 25, 17,48};
         for (int i = 0; i < arr.length; i++){
             int flag = 0;                               // large data set
---
+++ 2. Java Data Structure/1. Arrays/src/A4_MergeSort.java
@@ -0,0 +1,25 @@
+public class A4_MergeSort {
+    public static void main(String[] args) {
+        /*
+        Merge Sort is a Divide and Conquer sorting algorithm that:
+        Divides the array into two halves
+        Recursively sorts both halves
+        Merges the sorted halves into one sorted array
+
+        ⚙️ How It Works
+        Example: [38, 27, 43, 3, 9, 82, 10]
+        Step 1: Divide
+        [38,27,43]   [3,9,82,10]
+        Step 2: Divide Until Single Elements
+        [38] [27] [43] [3] [9] [82] [10]
+        Step 3: Merge & Sort
+        [27,38] → [27,38,43]
+        [3,9] → [3,9,10,82]
+        Final → [3,9,10,27,38,43,82]
+
+        ⏱ Time & Space Complexity
+        Best Case: O(n log n), Average Case: O(n log n), Worst Case: O(n log n), Space Complexity: O(n)
+         */
+
+    }
+}
