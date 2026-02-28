94e5bf647477fb28aec0fa65df5ca510e71b3ffe
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-28 22:28:44 +0530

    Create JournalEntry.java

---
+++ 4. Java Spring Boot/5.Lombok-Dependency/src/main/java/SpringBoot/entity/JournalEntry.java
@@ -0,0 +1,22 @@
+package SpringBoot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Document(collection = "Lombok_entries")
+public class JournalEntry {
+
+    @Id
+    private ObjectId id;
+
+    private String title;
+    private String content;
+    private LocalDateTime date;
+
+}
