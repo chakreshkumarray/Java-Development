67f70b0c8156be3f27d81ae1ed949285cebe55bb
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-02-28 07:39:58 +0530

    Create S1_BubbleSort.java

---
+++ 2. Java Data Structure/2. Strings/src/S1_BubbleSort.java
@@ -0,0 +1,26 @@
+public class S1_BubbleSort {
+    public static void main(String[] args) {
+
+        String[] name = {"Ambreesh","Dheeraj","Shivam","Anil","Shubham"};
+
+        for (int i = 0; i < name.length; i++){
+            int flag = 0;
+            for (int j = 0; j < name.length - 1 - i; j++){
+                if (name[j].compareTo(name[j + 1]) > 0){ // ASCII Lexicographically value check
+                    String temp = name[j];
+                    name[j] = name[j + 1];
+                    name[j + 1] = temp;
+                    flag = 1;
+                }
+            }
+            if (flag == 0){
+                break;
+            }
+        }
+
+        for (String i : name){
+            System.out.println(i);
+        }
+
+    }
+}
