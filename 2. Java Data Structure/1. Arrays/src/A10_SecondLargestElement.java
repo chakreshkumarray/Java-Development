bf1e3fe0a68b5df28b0a45613fb67957bffe46fe
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 07:47:36 +0530

    Update A10_SecondLargestElement.java

--- 2. Java Data Structure/1. Arrays/src/A10_SecondLargestElement.java
+++ 2. Java Data Structure/1. Arrays/src/A10_SecondLargestElement.java
@@ -16,5 +16,23 @@ public class A10_SecondLargestElement {
         }
         System.out.println("Second Largest Element is: "+arr[1]);
 
+        // Other Method
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest){
+                secondLargest = arr[i];
+            }
+        }
+        if (secondLargest == Integer.MIN_VALUE){
+            System.out.println("There is no second largest element in the list..");
+        }
+        else {
+            System.out.println("Second Largest Element : " + secondLargest);
+        }
     }
 }
