8c8593c385037f7ffc1eb5ce8bb3976b1c014362
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 07:18:06 +0530

    Create JournalEntryService.java

---
+++ 4. Java Spring Boot/6.Link-Two-Collections/src/main/java/SpringBootApplication6/Link/Two/Collections/service/JournalEntryService.java
@@ -0,0 +1,49 @@
+package SpringBootApplication6.Link.Two.Collections.service;
+
+import SpringBootApplication6.Link.Two.Collections.entity.JournalEntry;
+import SpringBootApplication6.Link.Two.Collections.entity.User;
+import SpringBootApplication6.Link.Two.Collections.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public void saveEntry(JournalEntry journalEntry, String userName){
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userService.saveEntry(user);
+    }
+
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id, String userName){
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(x ->x.getId().equals(id));
+        userService.saveEntry(user);
+        journalEntryRepository.deleteById(id);
+    }
+}
