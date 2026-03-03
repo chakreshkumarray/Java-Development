93ac0b3072c9fe4aafe4f3ae0acd68c6fa19e3de
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 07:58:10 +0530

    Update A13_KthSmallestElement.java

--- 2. Java Data Structure/1. Arrays/src/A13_KthSmallestElement.java
+++ 2. Java Data Structure/1. Arrays/src/A13_KthSmallestElement.java
@@ -1,7 +1,22 @@
 public class A13_KthSmallestElement {
     public static void main(String[] args) {
 
-        // Kth Smallest Element
-        
+        // Kth Smallest Element, given index
+        int[] arr = {5, 8, 12, 7, 6, 2, 4};
+
+        int k = 3;
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            if (i == k-1){
+                System.out.println("Third Smallest Element is: "+arr[i]);
+                break;
+            }
+        }
     }
 }
