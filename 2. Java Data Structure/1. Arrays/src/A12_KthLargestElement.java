1826c0a315bf31ee35ecc6c579be847ef1c8ba6c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 07:58:16 +0530

    Update A12_KthLargestElement.java

--- 2. Java Data Structure/1. Arrays/src/A12_KthLargestElement.java
+++ 2. Java Data Structure/1. Arrays/src/A12_KthLargestElement.java
@@ -2,6 +2,21 @@ public class A12_KthLargestElement {
     public static void main(String[] args) {
 
         // Kth Largest Element
+        int[] arr = {5, 8, 12, 7, 6, 2, 4};
 
+        int k = 4;
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            if (i == k-1){
+                System.out.println("Fourth largest Element is: "+arr[i]);
+                break;
+            }
+        }
     }
 }
