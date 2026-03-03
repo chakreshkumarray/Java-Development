736500e9105a8208a5839d367b275785b94959ed
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 07:18:19 +0530

    Create ApplicationTests.java

---
+++ 4. Java Spring Boot/6.Link-Two-Collections/src/test/java/SpringBootApplication6/Link/Two/Collections/ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootApplication6.Link.Two.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
