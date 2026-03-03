42ddb46be55825ab884c85de9e6ca7cfeefd52e6
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 12:29:04 +0530

    Update A17_DuplicateElement_Set.java

--- 2. Java Data Structure/1. Arrays/src/A17_DuplicateElement_Set.java
+++ 2. Java Data Structure/1. Arrays/src/A17_DuplicateElement_Set.java
@@ -1,6 +1,8 @@
 public class A17_DuplicateElement_Set {
     public static void main(String[] args) {
 
+        // Find Duplicate Element in Array by Using Set Interface
+        // Set don't allow duplicate element
         
     }
 }
