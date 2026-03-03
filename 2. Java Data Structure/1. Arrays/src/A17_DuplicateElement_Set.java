d4c2129b9f9e01447fe2fe90cb5e38ce4c553b74
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 20:43:12 +0530

    Update A17_DuplicateElement_Set.java

--- 2. Java Data Structure/1. Arrays/src/A17_DuplicateElement_Set.java
+++ 2. Java Data Structure/1. Arrays/src/A17_DuplicateElement_Set.java
@@ -1,8 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class A17_DuplicateElement_Set {
     public static void main(String[] args) {
 
         // Find Duplicate Element in Array by Using Set Interface
         // Set don't allow duplicate element
-        
+        // This use where element repeat only one time otherwise it will through error.
+
+        int[] arr = {3, 5, 4, 3, 2, 2, 1};
+
+        Set<Integer> s = new HashSet<>();
+
+        for (int number : arr){
+
+            if (s.add(number) == false){
+
+                System.out.println(number);
+            }
+        }
+
     }
 }
