5d3d5ae4d17f37744c2d2f140ba53281148f0d00
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-03 07:47:32 +0530

    Update A11_SecondSmallestElement.java

--- 2. Java Data Structure/1. Arrays/src/A11_SecondSmallestElement.java
+++ 2. Java Data Structure/1. Arrays/src/A11_SecondSmallestElement.java
@@ -15,5 +15,24 @@ public class A11_SecondSmallestElement {
             }
         }
         System.out.println("Second Smallest Number is : "+arr[1]);
+
+        // Other Method
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] < smallest){
+                secondSmallest = smallest;
+                smallest = arr[i];
+            }
+            else if (arr[i] < secondSmallest && arr[i] != smallest){
+                secondSmallest = arr[i];
+            }
+        }
+        if (secondSmallest == Integer.MAX_VALUE){
+            System.out.println("There is no second smallest element in the list..");
+        }
+        else {
+            System.out.println("Second Smallest Element : " + secondSmallest);
+        }
     }
 }
