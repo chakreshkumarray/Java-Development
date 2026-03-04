fc3a6fc989d4495c59247bebf33f696f6d536127
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-05 03:57:24 +0530

    Create JournalEntryController.java

---
+++ 4. Java Spring Boot/8.MongoDB-Atlas-Connection/src/main/java/SpringBootApplication8/MongoDB/Atlas/Connection/controller/JournalEntryController.java
@@ -0,0 +1,73 @@
+package SpringBootApplication8.MongoDB.Atlas.Connection.controller;
+
+import SpringBootApplication8.MongoDB.Atlas.Connection.entity.JournalEntry;
+import SpringBootApplication8.MongoDB.Atlas.Connection.entity.User;
+import SpringBootApplication8.MongoDB.Atlas.Connection.service.JournalEntryService;
+import SpringBootApplication8.MongoDB.Atlas.Connection.service.UserService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{userName}")
+    public ResponseEntity<?> getAllJournalEntriesOfUser(@PathVariable String userName){
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> all = user.getJournalEntries();
+        if (all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("{userName}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName){
+        try {
+            journalEntryService.saveEntry(myEntry,userName);
+            return new ResponseEntity<>(myEntry,HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("id/{myId}")
+    public ResponseEntity<?> getJournalEntryById(@PathVariable ObjectId myId){
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+        if (journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("id/{userName}/{myId}")
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId, @PathVariable String userName){
+        journalEntryService.deleteById(myId,userName);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("id/{userName}/{myId}")
+    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry,
+                                               @PathVariable String userName){
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
+        if (old != null){
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.equals("") ? newEntry.getContent() : old.getContent());
+            return new ResponseEntity<>(old,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
