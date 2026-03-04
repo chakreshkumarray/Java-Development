ae0dd57d0916d5af5f3633844004ca33e5d04869
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-04 05:36:31 +0530

    Create Application.java

---
+++ 4. Java Spring Boot/7.Transaction-Annotation/src/main/java/SpringBootApplication7/Transaction/Annotation/Application.java
@@ -0,0 +1,13 @@
+package SpringBootApplication7.Transaction.Annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
