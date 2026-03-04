da59f8e0a9ac423d1755c787368390bb6684c106
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-04 05:36:47 +0530

    Create UserController.java

---
+++ 4. Java Spring Boot/7.Transaction-Annotation/src/main/java/SpringBootApplication7/Transaction/Annotation/controller/UserController.java
@@ -0,0 +1,40 @@
+package SpringBootApplication7.Transaction.Annotation.controller;
+
+import SpringBootApplication7.Transaction.Annotation.entity.User;
+import SpringBootApplication7.Transaction.Annotation.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAll(){
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user){
+        userService.saveEntry(user);
+    }
+
+    @PutMapping("/{userName}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName){
+        User userInDb = userService.findByUserName(userName);
+        if (userInDb != null){
+            userInDb.setUserName(user.getUserName());
+            userInDb.setPassword(user.getPassword());
+            userService.saveEntry(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
