987bb503bc027e9e5f07e5f51207cb0d377c3b5e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-05 03:58:56 +0530

    Create ApplicationTests.java

---
+++ 4. Java Spring Boot/8.MongoDB-Atlas-Connection/src/test/java/SpringBootApplication8/MongoDB/Atlas/Connection/ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootApplication8.MongoDB.Atlas.Connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
