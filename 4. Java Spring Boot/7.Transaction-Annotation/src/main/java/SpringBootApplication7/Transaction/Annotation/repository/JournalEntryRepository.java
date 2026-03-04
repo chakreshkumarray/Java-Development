873946af3e277b2e33bce600dd9164dba6e853bc
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-04 05:37:50 +0530

    Create JournalEntryRepository.java

---
+++ 4. Java Spring Boot/7.Transaction-Annotation/src/main/java/SpringBootApplication7/Transaction/Annotation/repository/JournalEntryRepository.java
@@ -0,0 +1,9 @@
+package SpringBootApplication7.Transaction.Annotation.repository;
+
+import SpringBootApplication7.Transaction.Annotation.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
