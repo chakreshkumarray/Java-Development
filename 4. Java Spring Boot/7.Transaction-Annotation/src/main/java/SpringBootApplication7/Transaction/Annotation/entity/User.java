9db6e7f30c2b55fbc4d57080e1d1e8dd1fd0e05e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-04 05:37:05 +0530

    Create User.java

---
+++ 4. Java Spring Boot/7.Transaction-Annotation/src/main/java/SpringBootApplication7/Transaction/Annotation/entity/User.java
@@ -0,0 +1,30 @@
+package SpringBootApplication7.Transaction.Annotation.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Document(collection = "user_entries")
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
+    @NonNull
+    private String userName;
+
+    @NonNull
+    private String password;
+
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+}
