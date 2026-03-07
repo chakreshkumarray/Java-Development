dfaf6878218ceaeba5ec6a43469d20eac6a06598
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-07 07:33:08 +0530

    Create R2_SumOf_NaturalNumber.java

---
+++ 1. Java Basics Tutorial/17. Recursion/src/R2_SumOf_NaturalNumber.java
@@ -0,0 +1,13 @@
+public class R2_SumOf_NaturalNumber {
+    public static void main(String[] args) {
+
+        System.out.println(SumNatural(5));
+    }
+
+    public static int SumNatural(int num){
+        if(num == 1){      // 5+4+3+2+1
+            return 1;
+        }
+        return num+SumNatural(num - 1);
+    }
+}
