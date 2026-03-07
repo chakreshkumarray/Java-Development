76d26b95fee6c958666ee79020b80491b6533461
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-07 06:26:25 +0530

    Update A29_DeleteElement.java

--- 2. Java Data Structure/1. Arrays/src/A29_DeleteElement.java
+++ 2. Java Data Structure/1. Arrays/src/A29_DeleteElement.java
@@ -2,6 +2,18 @@ public class A29_DeleteElement {
     public static void main(String[] args) {
 
         // How to Remove specific element from an array
-
+        int[] arr = {10, 40, 30, 50, 60, 20};
+        int delete = 30;
+        for (int i = 0; i < arr.length; i++){
+            if (delete == arr[i]){
+                for (int j = i; j < arr.length - 1; j++){
+                    arr[j] = arr[j + 1];
+                }
+                break;
+            }
+        }
+        for (int i : arr){
+            System.out.println(i);
+        }
     }
 }
