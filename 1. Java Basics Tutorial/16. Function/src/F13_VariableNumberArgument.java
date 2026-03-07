3844c8b37297437ea644aac6729e3d0f1829c844
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-07 07:30:17 +0530

    Create F13_VariableNumberArgument.java

---
+++ 1. Java Basics Tutorial/16. Function/src/F13_VariableNumberArgument.java
@@ -0,0 +1,15 @@
+public class F13_VariableNumberArgument {
+    public static void main(String[] args) {
+
+        System.out.println(sum(1,0,5,5));
+
+    }
+
+    public static int sum(int... a){
+        int sum = 0;
+        for(int i : a){
+            sum = sum + i;
+        }
+        return sum;
+    }
+}
