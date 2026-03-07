3bcb41c88a9395f5aaa08d9f32d032af35365c2b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-07 07:29:29 +0530

    Update Car.java

--- 1. Java Basics Tutorial/15. OOPs Concept Overview/src/Car.java
+++ 1. Java Basics Tutorial/15. OOPs Concept Overview/src/Car.java
@@ -8,14 +8,14 @@ public class Car {
     int speed;
 
     public void accelerate(int increment){
-        speed = speed+increment;
+        speed = speed + increment;
         System.out.println("Car is Running");
     }
 
     public void brake(int decrement){
-        speed = speed-decrement;
-        if(speed<=0){
-            speed=0;
+        speed = speed - decrement;
+        if(speed <= 0){
+            speed = 0;
             System.out.println("break");
         }else {
             System.out.println("Slow");
