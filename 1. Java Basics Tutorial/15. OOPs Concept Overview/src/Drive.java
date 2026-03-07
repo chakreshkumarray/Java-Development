0f05adf8efe2c206cef2e42380d1b6aa1a76a7d1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-07 07:29:40 +0530

    Update Drive.java

--- 1. Java Basics Tutorial/15. OOPs Concept Overview/src/Drive.java
+++ 1. Java Basics Tutorial/15. OOPs Concept Overview/src/Drive.java
@@ -1,12 +1,12 @@
 public class Drive {
     public static void main(String[] args) {
 
-        Car Car = new Car(); // object creation of Car_Class
-        Car.color="Pink";
-        Car.brand="Tata";
-        Car.model="Safari";
-        Car.year=2024;
-        Car.speed=40;
+        Car Car = new Car(); // object creation of Car Class
+        Car.color = "Pink";
+        Car.brand = "Tata";
+        Car.model = "Safari";
+        Car.year = 2024;
+        Car.speed = 40;
         System.out.println(Car.color);
         System.out.println(Car.brand);
         System.out.println(Car.model);
