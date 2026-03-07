ee366f05267d0154b64a6ba5dd845b558a27b102
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-07 07:33:23 +0530

    Create R4_FibonacciSeries.java

---
+++ 1. Java Basics Tutorial/17. Recursion/src/R4_FibonacciSeries.java
@@ -0,0 +1,17 @@
+public class R4_FibonacciSeries {
+    public static void main(String[] args) {
+
+        System.out.println(Fibonacci(5));
+        System.out.println(Fibonacci(8));
+    }
+
+    public static int Fibonacci(int n){
+        if (n == 1){
+            return 0;
+        }
+        if (n == 0){
+            return 1;
+        }
+        return Fibonacci(n-1) + Fibonacci(n-2);
+    }
+}
