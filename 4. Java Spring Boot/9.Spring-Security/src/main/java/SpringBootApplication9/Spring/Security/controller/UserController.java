7adcf2522d7e9054cd2dacc47c0241a9f33ee044
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-08 09:57:12 +0530

    Create UserController.java

---
+++ 4. Java Spring Boot/9.Spring-Security/src/main/java/SpringBootApplication9/Spring/Security/controller/UserController.java
@@ -0,0 +1,39 @@
+package SpringBootApplication9.Spring.Security.controller;
+
+import SpringBootApplication9.Spring.Security.entity.User;
+import SpringBootApplication9.Spring.Security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAll(){
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user){
+        userService.saveEntry(user);
+    }
+
+    @PutMapping("/{userName}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName){
+        User userInDb = userService.findByUserName(userName);
+        if (userInDb != null){
+            userInDb.setUserName(user.getUserName());
+            userInDb.setPassword(user.getPassword());
+            userService.saveEntry(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
