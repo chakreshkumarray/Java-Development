b02d6a4f3d6c2e7c4732b55929380f3285819989
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-08 09:56:22 +0530

    Delete Test.java

--- 1. Java Basics Tutorial/20. Constructor/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-
-        Student student = new Student("Ck","VBSPU",12,15,3500);
-
-        System.out.println(student.getName());
-        System.out.println(student.getAge());
-    }
-}
