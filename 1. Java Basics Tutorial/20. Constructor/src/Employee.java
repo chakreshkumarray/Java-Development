615a3b062d88d12b66a8575a82ea515da0657b76
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-08 10:15:05 +0530

    Update Employee.java

--- 1. Java Basics Tutorial/20. Constructor/src/Employee.java
+++ 1. Java Basics Tutorial/20. Constructor/src/Employee.java
@@ -3,5 +3,15 @@ public class Employee {
     String Name;
     int emp_id;
 
+    // here default constructor
+    // also use super();
 
+    /*
+       No Arg Constructor
+
+       Employee(){
+
+       }
+
+     */
 }
