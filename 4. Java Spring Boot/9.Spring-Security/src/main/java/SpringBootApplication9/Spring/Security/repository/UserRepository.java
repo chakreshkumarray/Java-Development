535a1675a0f27250807de628546e0cab94478354
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-08 09:57:25 +0530

    Create UserRepository.java

---
+++ 4. Java Spring Boot/9.Spring-Security/src/main/java/SpringBootApplication9/Spring/Security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package SpringBootApplication9.Spring.Security.repository;
+
+import SpringBootApplication9.Spring.Security.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    User findByUserName(String username);
+}
