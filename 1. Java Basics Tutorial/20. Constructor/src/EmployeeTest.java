2bccbdc6bae70a10bbd17ae399b966272548261d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-08 09:56:11 +0530

    Create EmployeeTest.java

---
+++ 1. Java Basics Tutorial/20. Constructor/src/EmployeeTest.java
@@ -0,0 +1,10 @@
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+        employee.Name = "Ck";
+        employee.emp_id = 123;
+        System.out.println(employee.Name);
+        System.out.println(employee.emp_id);
+    }
+}
