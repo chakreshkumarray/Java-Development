7f74dcc491ba411b40ac774f974dc83ae8959484
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-11 08:13:48 +0530

    Update Test.java

--- 1. Java Basics Tutorial/21. Inheritance/src/Single_Inheritance/Test.java
+++ 1. Java Basics Tutorial/21. Inheritance/src/Single_Inheritance/Test.java
@@ -2,7 +2,12 @@ package Single_Inheritance;
 
 public class Test {
   public static void main(String[] args) {
-    
+
+      Animal animal = new Animal();
+      Dog dog = new Dog();
+      dog.sayHello();
+      animal.eat();
+
   }
   
 }
