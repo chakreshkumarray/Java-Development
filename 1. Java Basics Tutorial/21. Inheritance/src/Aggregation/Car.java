f001d0c51f08ce540ed31655b51af991b82e4ae4
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-03-19 14:44:01 +0530

    Create Car.java

---
+++ 1. Java Basics Tutorial/21. Inheritance/src/Aggregation/Car.java
@@ -0,0 +1,15 @@
+package Aggregation;
+
+public class Car {
+
+    /*
+    Aggregation is weak bonding --> low bonding
+
+    Car does not depend upon Car, Car can run without Music Player
+     */
+
+    public void MusicPlayer(){
+        System.out.println("Open Music");
+    }
+
+}
