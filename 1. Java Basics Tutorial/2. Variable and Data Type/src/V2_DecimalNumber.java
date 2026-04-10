62e11296b4ad81e8b2859db3c2974e78d3f5728c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-10 12:37:51 +0530

    Update V2_DecimalNumber.java

--- 1. Java Basics Tutorial/2. Variable and Data Type/src/V2_DecimalNumber.java
+++ 1. Java Basics Tutorial/2. Variable and Data Type/src/V2_DecimalNumber.java
@@ -12,3 +12,18 @@ public class V2_DecimalNumber {
         System.out.println(Double.MAX_VALUE);
     }
 }
+/*
+Q. What is Float and Double Data types?
+Ans. These are used to store decimal (fractional) numbers.
+     1. Float
+        Size: 4 bytes (32 bits)
+        Precision: ~6–7 decimal digits
+        Must use f or F at the end
+        Example: float f = 3.14f;
+
+     2. Double
+        Size: 8 bytes (64 bits)
+        Precision: ~15–16 decimal digits
+        No suffix needed
+        Example: double d = 3.14159265359;
+ */
