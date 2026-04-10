c30f9aee532a042c4358abaef5c6172680c72782
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-10 14:23:30 +0530

    Update B1_AND.java

--- 1. Java Basics Tutorial/5. Bitwise Operator/src/B1_AND.java
+++ 1. Java Basics Tutorial/5. Bitwise Operator/src/B1_AND.java
@@ -8,3 +8,22 @@ public class B1_AND {
         System.out.println("And Of a and b :"+AND);
     }
 }
+/*
+Q. What is Bitwise Operator?
+Ans. Bitwise operators are used to perform operations directly on the binary (bit-level) representation of numbers.
+
+| Operator | Name        | Example | Result |   |
+| -------- | ----------- | ------- | ------ | - |
+| `&`      | AND         | 5 & 3   | 1      |   |
+| `|`      | OR          | 5 | 3   | 7      |   |
+| `^`      | XOR         | 5 ^ 3   | 6      |   |
+| `~`      | NOT         | ~5      | -6     |   |
+| `<<`     | Left Shift  | 5 << 1  | 10     |   |
+| `>>`     | Right Shift | 5 >> 1  | 2      |   |
+
+Bitwise AND(&)
+5 = 101
+3 = 011
+---------
+    001 = 1
+ */
