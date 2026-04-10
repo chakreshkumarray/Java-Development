ea9edd5e4f853dba03e18114882ad1f6c399e153
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-10 11:09:15 +0530

    Update F1_Morning.java

--- 1. Java Basics Tutorial/1. First Program/src/F1_Morning.java
+++ 1. Java Basics Tutorial/1. First Program/src/F1_Morning.java
@@ -3,3 +3,59 @@ public class F1_Morning {
         System.out.println("Morning");
     }
 }
+/*
+Q. What is Java?
+Ans. Java is a high-level, object-oriented, platform-independent programming language
+     used to build secure, scalable, and robust applications. It follows the principle
+     of “write once, run anywhere” because Java code is compiled into bytecode that runs
+     on the Java Virtual Machine (JVM).
+
+Q. What is meaning of High-Level?
+Ans. “High-level” (in programming) means a language that is easy for humans to read, write, and understand,
+      because it is closer to natural language and far from machine language (0s and 1s).
+      A high-level language is a programming language that uses simple, human-readable syntax and hides complex
+      hardware details.
+
+Q. What is meaning of object-oriented?
+Ans. “Object-oriented” means a way of programming where we organize code using objects and classes,
+      instead of just functions or instructions.
+      Object-oriented means designing programs using objects, which contain data (variables) and behavior (methods).
+
+Q. What is meaning of Platform-independent?
+Ans. Platform-independent means a program can run on any operating system (Windows, Linux, Mac) without changing its code.
+     “Write Once, Run Anywhere”
+     Java code is compiled into bytecode (.class file)
+     This bytecode runs on the Java Virtual Machine (JVM)
+     JVM is available for every OS
+
+Q. What is meaning of secure?
+Ans. Secure means the application protects data and prevents unauthorized access.
+     No direct memory access (no pointers)
+     Bytecode verification
+     Built-in security features
+
+     Example: Banking apps keep user data safe.
+
+Q. What is meaning of Scalable?
+Ans. Scalable means the application can handle increasing users or data without performance issues.
+     Works well for 100 users
+     Also works well for 1 lakh users
+
+     Example: Instagram handling millions of users.
+
+Q. What is meaning of Robust Application?
+Ans. Robust means the application is strong, stable, and can handle errors without crashing.
+     Strong exception handling
+     Type checking
+     Memory management
+
+     Example: Even if an error occurs, the app continues running.
+
+     ✅ 🔹 1. Banking Application 🏦
+     Example: When you transfer money.
+
+     If network fails, money is not lost
+     Either transaction completes or rolls back
+     Shows proper error message
+     This means the system handles errors safely → Robust
+ */
