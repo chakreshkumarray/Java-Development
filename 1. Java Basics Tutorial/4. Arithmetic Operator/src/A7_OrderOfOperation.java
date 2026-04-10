2497db20ef09f5711a0dd9333884da60166437ca
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-10 13:55:48 +0530

    Update A7_OrderOfOperation.java

--- 1. Java Basics Tutorial/4. Arithmetic Operator/src/A7_OrderOfOperation.java
+++ 1. Java Basics Tutorial/4. Arithmetic Operator/src/A7_OrderOfOperation.java
@@ -5,3 +5,16 @@ public class A7_OrderOfOperation {
         System.out.println(8 % 23 / 5 * 6 + 4 - 3);
     }
 }
+/*
+The order of operations tells you how an expression is evaluated step by step when multiple operators are used together.
+Standard Order (High → Low Priority)
+Parentheses ()
+Unary operators ++, --, !
+Multiplication / Division / Modulus *, /, %
+Addition / Subtraction +, -
+Relational operators <, >, <=, >=
+Equality operators ==, !=
+Logical AND &&
+Logical OR ||
+Assignment =, +=, -=, ...
+ */
