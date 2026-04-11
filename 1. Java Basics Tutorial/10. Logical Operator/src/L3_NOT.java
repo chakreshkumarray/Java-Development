ebe62af6bf8dcbcff97ad021629f58ba5244a84f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-11 13:14:14 +0530

    Update L3_NOT.java

--- 1. Java Basics Tutorial/10. Logical Operator/src/L3_NOT.java
+++ 1. Java Basics Tutorial/10. Logical Operator/src/L3_NOT.java
@@ -1,12 +1,12 @@
 public class L3_NOT {
     public static void main(String[] args) {
 
-        int a=2;
-        int b=3;
+        int a = 2;
+        int b = 3;
         System.out.println(! ( a < b ));
 
-        char c='C';
-        char d='o';
+        char c = 'C';
+        char d = 'o';
         System.out.println(! ( c > d ));
     }
 }
