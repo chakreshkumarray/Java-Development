2a72be3583cec678827e1bedc5fac3b29c93a749
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-11 13:13:27 +0530

    Update R5_GreaterThanEqual.java

--- 1. Java Basics Tutorial/9. Relational Operator/src/R5_GreaterThanEqual.java
+++ 1. Java Basics Tutorial/9. Relational Operator/src/R5_GreaterThanEqual.java
@@ -3,12 +3,12 @@ public class R5_GreaterThanEqual {
 
         int a = 15;
         int b = 24;
-        System.out.println(a>=b);
+        System.out.println(a >= b);
 
         char str1 = 'f'; // 120
         char str2 = 'N'; // 78
-        System.out.println('f'+0);
-        System.out.println('N'+0);
+        System.out.println('f' + 0);
+        System.out.println('N' + 0);
         System.out.println(str1 >= str2);
 
     }
