23620645ae62dd869c53b411ef37b03255922b6d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-11 21:09:40 +0530

    Update Animal.java

--- 1. Java Basics Tutorial/18. Package and Class Path/src/Animal.java
+++ 1. Java Basics Tutorial/18. Package and Class Path/src/Animal.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Animal {
-  
+
+    Scanner input = new Scanner(System.in);
 }
+/*
+🔷 1. What is a Package in Java?
+
+A package is a folder (directory) that groups related classes together.
+
+👉 It helps organize your code like files in folders.
+
+🔷 2. What is Classpath?
+
+Classpath is a path (location) where Java looks for:
+.class files
+packages
+libraries
+
+👉 It tells Java:
+👉 "Where should I find the compiled classes?
+ */
