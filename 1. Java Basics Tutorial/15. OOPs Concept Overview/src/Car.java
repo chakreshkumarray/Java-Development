8393d09e71357c3ad9df2b47b61f8d78c07339a7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-11 21:09:51 +0530

    Update Car.java

--- 1. Java Basics Tutorial/15. OOPs Concept Overview/src/Car.java
+++ 1. Java Basics Tutorial/15. OOPs Concept Overview/src/Car.java
@@ -23,3 +23,27 @@ public class Car {
     }
 
 }
+/*
+OOPs (Object-Oriented Programming System) is a programming concept where we organize code
+using objects and classes instead of just functions and logic.
+
+1. Class
+A class is a blueprint/template.
+
+2. Object
+An object is an instance of a class.
+
+3. Encapsulation (Data Hiding + Security)
+Wrapping data + methods into one unit (class) and restricting access.
+
+4. Inheritance (Code Reusability)
+One class acquires properties of another class.
+
+5. Polymorphism (Many Forms)
+➤ Compile-time (Method Overloading)
+➤ Runtime (Method Overriding)
+
+6. Abstraction (Hiding Implementation)
+Using Abstract Class
+Using Interface
+ */
