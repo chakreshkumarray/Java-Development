23a4c5af3af54bfc44093c714e2a4478ee3e4849
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-11 13:13:12 +0530

    Update L1_ForLoop.java

--- 1. Java Basics Tutorial/13. Loop/src/L1_ForLoop.java
+++ 1. Java Basics Tutorial/13. Loop/src/L1_ForLoop.java
@@ -2,7 +2,7 @@ public class L1_ForLoop {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 10; i++){
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
 
     }
