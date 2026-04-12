5314be16ee91818d425f70888d4a030f1320922c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 20:42:59 +0530

    Create Test.java

---
+++ 1. Java Basics Tutorial/32. Enumeration/src/E3_Month/Test.java
@@ -0,0 +1,12 @@
+package E3_Month;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Month month = Month.March;
+        month.display();
+        month.getLower();
+
+
+    }
+}
