4f57594c552441b6e8afe03e43fa5947f8b4f8d1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 20:43:35 +0530

    Create Test.java

---
+++ 1. Java Basics Tutorial/32. Enumeration/src/E7_SwitchCase/Test.java
@@ -0,0 +1,22 @@
+package E7_SwitchCase;
+
+public class Test {
+    public static void main(String[] args) {
+
+        SwitchCase years = SwitchCase.lk;
+        switch (years){
+            case ak -> {
+                System.out.println("this is my name");
+                break;
+            }
+            case lk -> {
+                System.out.println("ak");
+                break;
+            }
+            default -> {
+                System.out.println("invalid name");
+            }
+        }
+
+    }
+}
