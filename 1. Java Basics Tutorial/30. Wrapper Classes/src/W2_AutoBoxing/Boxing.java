fda6d8977fa924fe120913aa8980d5715f4e8fc4
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 20:11:40 +0530

    Create Boxing.java

---
+++ 1. Java Basics Tutorial/30. Wrapper Classes/src/W2_AutoBoxing/Boxing.java
@@ -0,0 +1,13 @@
+package W2_AutoBoxing;
+
+public class Boxing {
+    public static void main(String[] args) {
+
+        int b = 12;
+        Integer a = 1; // AutoBoxing
+        // Behind the seen
+
+        Integer c = Integer.valueOf(12);
+
+    }
+}
