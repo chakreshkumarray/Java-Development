acd5bd38e38e8f65c984337c2188aa991df48847
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 20:42:36 +0530

    Create Day.java

---
+++ 1. Java Basics Tutorial/32. Enumeration/src/E1_Day/Day.java
@@ -0,0 +1,11 @@
+package E1_Day;
+
+public enum Day {
+
+    Sunday, Monday, Tuesday, Wednesday,
+    Thursday,Friday,Saturday
+}
+/*
+In Java, Enumeration (Enum) is a special type used to define a collection of constant values.
+It was introduced in Java 5 as a better alternative to static final constants.
+ */
