064c06fde510a68bca89f342ada21d2898623676
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 17:46:34 +0530

    Create Test.java

---
+++ 1. Java Basics Tutorial/28. Inner Classes/src/Topic3_Local_InnerClass/Test.java
@@ -0,0 +1,10 @@
+package Topic3_Local_InnerClass;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Hotel hotel = new Hotel("Chk",30,2);
+        hotel.reserveRoom("CK",2);
+
+    }
+}
