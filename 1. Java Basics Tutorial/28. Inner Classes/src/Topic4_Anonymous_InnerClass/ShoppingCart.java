cf2ffff489704d66272a9a4fdd57639d75811837
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 17:46:18 +0530

    Create ShoppingCart.java

---
+++ 1. Java Basics Tutorial/28. Inner Classes/src/Topic4_Anonymous_InnerClass/ShoppingCart.java
@@ -0,0 +1,15 @@
+package Topic4_Anonymous_InnerClass;
+
+public class ShoppingCart {
+
+    private final double totalAmount;
+
+    public ShoppingCart(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public void processPayment(Payment paymentMethod){
+        paymentMethod.Pay(totalAmount);
+    }
+
+}
