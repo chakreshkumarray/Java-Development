33ca5e12bff64bab33980051212a01072f827085
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 16:26:48 +0530

    Update BankMain.java

--- 1. Java Basics Tutorial/19. Encapsulation/src/BankMain.java
+++ 1. Java Basics Tutorial/19. Encapsulation/src/BankMain.java
@@ -12,3 +12,29 @@ public class BankMain {
         System.out.println(bank.getBalance());
     }
 }
+/*
+Q. What is Encapsulation (Deep Understanding) ?
+Ans. Encapsulation = Binding data (variables) + methods (functions) into a single unit (class) AND
+     controlling access to that data.
+
+     It is also called Data Hiding
+
+     Encapsulation is NOT just getters/setters.
+      It is about:
+      Protecting data
+      Controlling how data is accessed/modified
+      Maintaining object integrity
+
+Q. Why Encapsulation?
+Ans. Data Security -> Prevent direct access
+     Data Validation ->
+     Flexibility -> You can change internal logic without affecting users
+     Maintainability -> Clean and structured code
+
+Q. Encapsulation Rules (Important)
+Ans.  Make variables private
+      Provide public getter/setter methods
+      Add validation logic inside methods
+
+
+ */
