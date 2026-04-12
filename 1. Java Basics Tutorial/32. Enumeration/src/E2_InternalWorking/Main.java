e43c05924a068821ff6c217580d85a38963cf992
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 20:42:54 +0530

    Create Main.java

---
+++ 1. Java Basics Tutorial/32. Enumeration/src/E2_InternalWorking/Main.java
@@ -0,0 +1,11 @@
+package E2_InternalWorking;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Behind the seen
+        Day newDay = Day.mon;
+
+        System.out.println(newDay);
+    }
+}
