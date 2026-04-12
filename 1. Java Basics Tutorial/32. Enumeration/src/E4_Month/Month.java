784216bdbb7b25d147ad86e797718384e5e5af0c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-12 20:43:08 +0530

    Create Month.java

---
+++ 1. Java Basics Tutorial/32. Enumeration/src/E4_Month/Month.java
@@ -0,0 +1,13 @@
+package E4_Month;
+
+public enum Month {
+    Jan,Feb, March ,April ,May, June , July , Aug, Sep;
+
+
+    // Method
+    public void display(){
+        System.out.println("This month is "+this.name());
+    }
+
+
+}
