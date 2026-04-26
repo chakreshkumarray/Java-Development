e22299016288010928c1c7b815d36181901704e8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-04-26 19:53:27 +0530

    Audio to Text Converter Project Spring AI and Open AI

---
+++ 9. Project/Project - 4 Audion To Text Converter/Audio-To-Text-Converter/src/main/java/Audio/To/Text/Converter/AudioToTextConverterApplication.java
@@ -0,0 +1,13 @@
+package Audio.To.Text.Converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioToTextConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioToTextConverterApplication.class, args);
+	}
+
+}
---
+++ 9. Project/Project - 4 Audion To Text Converter/Audio-To-Text-Converter/src/test/java/Audio/To/Text/Converter/AudioToTextConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package Audio.To.Text.Converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioToTextConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
