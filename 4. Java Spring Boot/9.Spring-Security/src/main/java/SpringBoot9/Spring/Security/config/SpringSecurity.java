aff8b2febaade213844475fcc99b0e8d225b9dfd
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2026-05-20 18:56:21 +0530

    Update SpringSecurity.java

--- 4. Java Spring Boot/9.Spring-Security/src/main/java/SpringBoot9/Spring/Security/config/SpringSecurity.java
+++ 4. Java Spring Boot/9.Spring-Security/src/main/java/SpringBoot9/Spring/Security/config/SpringSecurity.java
@@ -1,10 +1,16 @@
 package SpringBoot9.Spring.Security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurity{
+public class SpringSecurity extends WebSecurityConfiguration{
 
+
+      protected void configure(HttpSecurity http) throws Exception {
+
+      }
 }
